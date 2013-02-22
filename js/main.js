7e40f56e7eeb5588c9c29fc2aa22bb6a6b36eda0
Sam Reid <reids@colorado.edu>
2013-02-21 23:32:00 -0700

    Added resize capabilities and initial scene

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-require( [], function () {
-  console.log( "hello" );
+require( [ "i18n!../nls/forces-and-motion-basics-strings"], function ( Strings ) {
+  var scene = new scenery.Scene( $( "#scene" ), {width: 200, height: 200, preferredSceneLayerType: scenery.LayerType.SVG} );
+  scene.addChild( new scenery.Text( "hi there", {x: 100, y: 100, fontSize: 42} ) );
+//  scene.addChild( arrow( tail, tip, {width: 50, fill: 'red', strokeWidth: 2 } ) );
+  scene.updateScene();
+
+  var resize = function () {
+    var width = $( window ).width();
+    var height = $( window ).height();
+
+    var scale = Math.min( width / 981, height / 644 );
+
+    scene.resize( width, height );
+    scene.updateScene();
+    console.log( "width", width, "height", height );
+    scene.setScale( scale );
+  };
+  $( window ).resize( resize );
+  resize();
 } );
