45a3f2667efc432275eeea0648f4d17bac7b87ed
Sam Reid <reids@colorado.edu>
2013-02-23 00:05:46 -0700

    Switched to image preloader using jquery.imagesloaded and load more tug of war images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 require( [ "view/View" ], function ( View ) {
-  var view = new View();
+  //Don't load the view until all images available.  Maybe future versions could optimize this by making the image loading dependencies more granular.
+  $( 'body' ).imagesLoaded( function ( $images, $proper, $broken ) {
+    var view = new View( $images );
+  } );
 } );
--- js/view/View.js
+++ js/view/View.js
@@ -1,41 +1,66 @@
 define( function ( require ) {
+
   var Strings = require( "i18n!../../nls/forces-and-motion-basics-strings" );
-  var cart = require( "image!images/cart.png" );
-  var pullFigureBlue0 = require( "image!images/pull_figure_BLUE_0.png" );
-  console.log( cart );
 
-  function View() {
+  function View( $images ) {
 
-  };
-  console.log( Strings );
-  var scene = new scenery.Scene( $( "#scene" ), {width: 200, height: 200, preferredSceneLayerType: scenery.LayerType.SVG} );
-  scene.addChild( new scenery.Text( "hi there", {x: 100, y: 100, fontSize: 42} ) );
-  scene.addChild( new scenery.Image( cart, {x: 100, y: 100, fontSize: 42} ) );
-  var blueFigure = new scenery.Image( pullFigureBlue0, {x: 100, y: 100, fontSize: 42} );
-  blueFigure.addInputListener( new scenery.SimpleDragHandler( {allowTouchSnag: true} ) );
-  scene.addChild( blueFigure );
+    function getImage( name ) {
+      var selector = 'img[src^="images/' + name + '"]';
+      return $images.parent().find( selector )[0];
+    }
 
-  scene.initializeFullscreenEvents(); // sets up listeners on the document with preventDefault(), and forwards those events to our scene
-  scene.resizeOnWindowResize(); // the scene gets resized to the full screen size
+    console.log( Strings );
+    this.scene = new scenery.Scene( $( "#scene" ), {width: 200, height: 200, preferredSceneLayerType: scenery.LayerType.SVG} );
+    this.scene.addChild( new scenery.Text( "hi there", {x: 100, y: 100, fontSize: 42} ) );
+    this.scene.addChild( new scenery.Image( getImage( 'cart' ), {x: 100, y: 100, fontSize: 42} ) );
 
-  var resize = function () {
+    var blueImageNames = ['pull_figure_small_BLUE_0', 'pull_figure_small_BLUE_0', 'pull_figure_BLUE_0', 'pull_figure_lrg_BLUE_0'];
+    var redImageNames = ['pull_figure_small_RED_0', 'pull_figure_small_RED_0', 'pull_figure_RED_0', 'pull_figure_lrg_RED_0'];
+    var view = this;
+
+    function addImages( imageNames ) {
+      for ( var i = 0; i < imageNames.length; i++ ) {
+        var image = getImage( imageNames[i] );
+        console.log( image );
+        var imageNode = new scenery.Image( image, {x: 100, y: 100, fontSize: 42} );
+        imageNode.addInputListener( new scenery.SimpleDragHandler( {allowTouchSnag: true} ) );
+        view.scene.addChild( imageNode );
+      }
+    }
+
+    addImages.call( this, blueImageNames );
+    addImages.call( this, redImageNames );
+
+    this.scene.initializeFullscreenEvents(); // sets up listeners on the document with preventDefault(), and forwards those events to our scene
+    this.scene.resizeOnWindowResize(); // the scene gets resized to the full screen size
+
+    //Fit to the window and render the initial scene
+    $( window ).resize( this.resize );
+    this.resize();
+
+    //http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+    // place the rAF *before* the render() to assure as close to
+    // 60fps with the setTimeout fallback.
+    (function animloop() {
+      requestAnimFrame( animloop );
+      view.render();
+    })();
+  }
+
+  View.prototype.resize = function () {
     var width = $( window ).width();
     var height = $( window ).height();
 
     var scale = Math.min( width / 981, height / 644 );
 
-    scene.resize( width, height );
-    scene.setScale( scale );
-    render();
+    this.scene.resize( width, height );
+    this.scene.setScale( scale );
+    this.render();
   };
-  $( window ).resize( resize );
 
-  //Fit to the window and render the initial scene
-  resize();
-
-  function render() {
-    scene.updateScene();
-  }
+  View.prototype.render = function () {
+    this.scene.updateScene();
+  };
 
   window.requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
@@ -48,12 +73,5 @@ define( function ( require ) {
            };
   })();
 
-  //http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-  // place the rAF *before* the render() to assure as close to
-  // 60fps with the setTimeout fallback.
-  (function animloop() {
-    requestAnimFrame( animloop );
-    render();
-  })();
   return View;
 } );
