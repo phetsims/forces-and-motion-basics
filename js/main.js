22997483a125dc3b5f1cf6fa734c0b31416ebd44
Sam Reid <reids@colorado.edu>
2013-02-24 21:12:37 -0700

    fixed object dragging on ie9; added a loading screen overlay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 require( [ "view/View" ], function ( View ) {
   "use strict";
+
+  var useDebugDiv = false;
+  if ( useDebugDiv ) {
+    if ( typeof console != "undefined" ) {
+      if ( typeof console.log != 'undefined' ) {
+        console.olog = console.log;
+      }
+      else {
+        console.olog = function () {};
+      }
+    }
+
+    console.log = function ( message ) {
+      console.olog( message );
+      $( '#debugDiv' ).append( '<p>' + message + '</p>' );
+    };
+  }
+
   //Don't load the view until all images available.  Maybe future versions could optimize this by making the image loading dependencies more granular.
   $( 'body' ).imagesLoaded( function ( $images, $proper, $broken ) {
     var view = new View( $images );
+    $( "#overlay" ).remove();
+    $( "#images" ).remove();
+    if ( !useDebugDiv ) {
+      $( "debugDiv" ).remove();
+    }
   } );
 } );
