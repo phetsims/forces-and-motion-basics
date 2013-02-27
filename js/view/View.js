2cedebdfc7b126bed4a9e2ba2e86f0b45b212129
Sam Reid <reids@colorado.edu>
2013-02-26 22:05:18 -0700

    factored out an arrow function

--- js/view/View.js
+++ js/view/View.js
@@ -13,6 +13,7 @@ define( function ( require ) {
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var Property = require( 'PHETCOMMON/model/property/Property' );
   var watch = require( 'view/watch' );
+  var arrow = require( 'view/arrow' );
   var red = "red";
   var blue = "blue";
 
@@ -65,46 +66,6 @@ define( function ( require ) {
     this.scene = new Scene( $( "#scene" ), {width: 200, height: 200, allowDevicePixelRatioScaling: true} );
 
     this.scene.addChild( new Image( getImage( 'grass' ), {x: 13, y: 368} ) );
-
-    function arrow( tailX, tailY, tipX, tipY, tailWidth, headWidth, headHeight ) {
-      var arrowShape = new Shape();
-      if ( tipX == tailX && tipY == tailY ) {
-        return arrowShape;
-      }
-      var vector = new Vector2( tipX - tailX, tipY - tailY );
-      var xHatUnit = vector.normalized();
-      var yHatUnit = xHatUnit.rotated( Math.PI / 2 );
-      var length = vector.magnitude();
-
-      //Set up a coordinate frame that goes from the tail of the arrow to the tip.
-      function getPoint( xHat, yHat ) {
-        var x = xHatUnit.x * xHat + yHatUnit.x * yHat + tailX;
-        var y = xHatUnit.y * xHat + yHatUnit.y * yHat + tailY;
-        return new Vector2( x, y );
-      }
-
-      if ( headHeight > length / 2 ) {
-        headHeight = length / 2;
-      }
-
-      var tailLength = length - headHeight;
-      var points = [
-        getPoint( 0, tailWidth / 2 ),
-        getPoint( tailLength, tailWidth / 2 ),
-        getPoint( tailLength, headWidth / 2 ),
-        getPoint( length, 0 ),
-        getPoint( tailLength, -headWidth / 2 ),
-        getPoint( tailLength, -tailWidth / 2 ),
-        getPoint( 0, -tailWidth / 2 )
-      ];
-
-      arrowShape.moveTo( points[0].x, points[0].y );
-      var tail = _.tail( points );
-      _.each( tail, function ( element ) { arrowShape.lineTo( element.x, element.y ) } );
-      arrowShape.close();
-
-      return arrowShape;
-    }
 
     this.sumArrow = new Path( {shape: new Shape(), fill: '#7dc673', stroke: '#000000', lineWidth: 1} );
 
