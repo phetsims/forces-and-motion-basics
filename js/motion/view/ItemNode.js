c5cf8e8023b52e693d195c4dea62688e286a0fc3
Sam Reid <reids@colorado.edu>
2013-03-13 13:40:19 -0600

    added record/playback

--- js/motion/view/ItemNode.js
+++ js/motion/view/ItemNode.js
@@ -10,14 +10,15 @@ define( function( require ) {
   var Bounds2 = require( 'DOT/Bounds2' );
   var Inheritance = require( 'PHETCOMMON/util/Inheritance' );
   var Strings = require( "i18n!../../../nls/forces-and-motion-basics-strings" );
-  var swatch = require( 'motion/model/SwatchJS' ).swatch;
+  var WatchJS = require( 'watch' );
+  var watch = WatchJS.watch;
 
   function ItemNode( model, item, image ) {
     var itemNode = this;
     Node.call( this, {x: item.position.x, y: item.position.y, cursor: 'pointer'} );
     this.addChild( new Image( image ) );
     this.addInputListener( new SimpleDragHandler( {translate: function( options ) { item.position = options.position; }} ) );
-    swatch( item, 'position', function( p ) { itemNode.setTranslation( p ); } );
+    watch( item, 'position', function( a, b, p ) { itemNode.setTranslation( p ); } );
   }
 
   Inheritance.inheritPrototype( ItemNode, Node );
--- js/motion/view/MotionView.js
+++ js/motion/view/MotionView.js
@@ -2,6 +2,12 @@ define( function( require ) {
   "use strict";
   var ControlPanel = require( 'tugofwar/view/ControlPanel' );
   var MotionScenery = require( 'motion/view/MotionScenery' );
+  var WatchJS = require( 'watch' );
+  var watch = WatchJS.watch;
+  var callWatchers = WatchJS.callWatchers;
+  var playback = false;
+  var log = [];
+  var logIndex = 0;
 
   function MotionView( imageLoader, model, $tab ) {
     var view = this;
@@ -10,10 +16,47 @@ define( function( require ) {
 
     view.model = model;
 //    view.controlPanel = new ControlPanel( model, view );
+    var initialModel = jQuery.extend( true, {}, view.model );
+
+    //Update the model by setting values from the specified model
+    function setModel( src, dst ) {
+      //set initial model to model
+      for ( var obj in src ) {
+        var oldVal = dst[obj];
+        if ( typeof oldVal === 'number' || typeof oldVal === 'string' ) {
+          dst[obj] = src[obj];
+        }
+        callWatchers( dst, obj, "set", src[obj], oldVal );
+        if ( typeof src[obj] === 'object' ) {
+          setModel( src[obj], dst[obj] );
+        }
+      }
+    }
+
+    function playbackEvent() {
+      playback = true;
+
+//      console.log(JSON.stringify(view.model));
+      setModel( initialModel, view.model );
+//      view.model.items[0].position = {x: 0, y: 0};
+//      console.log(JSON.stringify(view.model));
+    }
+
+    var $resetAllButton = $( '.playback-button' );
+    $resetAllButton.bind( 'touchstart', playbackEvent );
+    $resetAllButton.bind( 'click', playbackEvent );
     view.scenery = new MotionScenery( model, view, $tab, imageLoader );
 
     view.model.on( 'reset-all', function() {
       view.resetAll();
+    } );
+
+//
+    watch( view.model, function( property, action, newValue, oldValue, path ) {
+////      console.log( "something changed", arguments, JSON.stringify( path ) );
+//
+      log.push( {path: path === undefined ? "root" : path, property: property, action: action, newValue: newValue, oldValue: oldValue } );
+////      console.log( JSON.stringify( log ) );
     } );
   }
 
@@ -25,8 +68,30 @@ define( function( require ) {
       this.scenery.updateForces();
     },
     step: function() {
-      this.model.step();
-      this.scenery.scene.updateScene();
+      if ( playback ) {
+        var m = this.model;
+        var obj = log[logIndex];
+        var path = obj.path;
+        if ( path === "root" ) {
+          m[obj.property] = obj.newValue;
+        }
+        else {
+          var item = m;
+          for ( var k = 0; k < path.length; k++ ) {
+            var pathElement = path[k];
+            item = item[pathElement];
+          }
+          item[obj.property] = obj.newValue;
+        }
+
+        this.scenery.scene.updateScene();
+        logIndex++;
+      }
+      else {
+        this.model.step();
+        this.scenery.scene.updateScene();
+      }
+
     }
   };
 
