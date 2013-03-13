9ed46b775950acec34a3b657cd0e54d26332758d
Sam Reid <reids@colorado.edu>
2013-03-12 22:54:36 -0600

    removed precision from x/y in the model

--- js/motion/model/MotionModel.js
+++ js/motion/model/MotionModel.js
@@ -15,13 +15,13 @@ define( function( require ) {
     running: true,
     userInfo: {name: "Larry", hair: "Curly", corners: 3},
     items: [
-      {image: 'fridge.png', weight: 100, x: 41.662522202486706, y: 476.3339253996446, dragging: false},
-      {image: 'crate.png', weight: 100, x: 195.38099467140327, y: 495.77975133214903, dragging: false},
-      {image: 'crate.png', weight: 100, x: 114.69271758436949, y: 496.923623445826, dragging: false},
-      {image: 'girl-standing.png', weight: 100, x: 632.8667850799291, y: 450.0248667850796, dragging: false},
-      {image: 'man-standing.png', weight: 100, x: 719.8010657193606, y: 365.37833037300175, dragging: false},
-      {image: 'trash-can.png', weight: 100, x: 851.3463587921843, y: 511.79396092362316, dragging: false},
-      {image: 'mystery-object-01.png', weight: 100, x: 796.4404973357015, y: 586.1456483126109, dragging: false}
+      {image: 'fridge.png', weight: 100, x: 41, y: 476, dragging: false},
+      {image: 'crate.png', weight: 100, x: 195, y: 495, dragging: false},
+      {image: 'crate.png', weight: 100, x: 114, y: 496, dragging: false},
+      {image: 'girl-standing.png', weight: 100, x: 632, y: 450, dragging: false},
+      {image: 'man-standing.png', weight: 100, x: 719, y: 365, dragging: false},
+      {image: 'trash-can.png', weight: 100, x: 851, y: 511, dragging: false},
+      {image: 'mystery-object-01.png', weight: 100, x: 796, y: 586, dragging: false}
     ], step: function() {
       var dt = 1;
       this.velocity = 1;
