9ac33b469e9836778814fa918097c387dd508f5d
Sam Reid <reids@colorado.edu>
2013-03-17 01:09:14 -0600

    added initial support for server-side record and playback using socket.io

--- js/motion/view/MotionView.js
+++ js/motion/view/MotionView.js
@@ -8,6 +8,7 @@ define( function( require ) {
   var log = [];
   var logIndex = 0;
   var playbackTime = 0;
+  var getLogEntry = false;
 
   function MotionView( imageLoader, model, $tab ) {
     var view = this;
@@ -65,8 +66,23 @@ define( function( require ) {
     $resetButton.bind( 'touchstart', reset );
     $resetButton.bind( 'click', reset );
 
+    if ( typeof io != 'undefined' ) {
+      var socket = io.connect( 'http://simian.colorado.edu:44100' );
+      socket.on( 'news', function( data ) {
+        console.log( data );
+      } );
+      if ( getLogEntry ) {
+        socket.emit( 'get log entry' );
+      }
+      socket.on( 'deliver log entry', function( logEntry ) {
+        console.log( "deliver log entry" );
+        console.log( logEntry );
+        log.push( logEntry );
+      } );
+    }
+
     watch( view.model, function( property, action, newValue, oldValue, path ) {
-      if ( !playback ) {
+      if ( !playback && !getLogEntry ) {
         if ( action !== 'set' ) {
           console.log( path, property, action, newValue );
         }
@@ -79,6 +95,10 @@ define( function( require ) {
 //        img.src="http://simian.colorado.edu/__utm.gif?"+param;
 ////        $.post( 'http://simian.colorado.edu/'+param);//origin error on chrome
         log.push( logItem );
+
+        if ( !getLogEntry && typeof socket !== "undefined" ) {
+          socket.emit( 'post log entry', logItem );
+        }
       }
     } );
   }
@@ -91,20 +111,20 @@ define( function( require ) {
       this.scenery.updateForces();
     },
     step: function() {
-      if ( playback ) {
+      if ( playback || getLogEntry ) {
         var m = this.model;
 
         while ( logIndex < log.length ) {
           //find any events that passed in this time frame
           var time = log[logIndex].time;
-          if ( time < playbackTime ) {
+          if ( time < playbackTime || getLogEntry ) {
 
 //            console.log( "playing back log index: " + logIndex + ", time=" + time, 'logentry = ', JSON.stringify( log[logIndex].newValue ) );
 
-            var obj = log[logIndex];
-            var path = obj.path;
+            var logEntry = log[logIndex];
+            var path = logEntry.path;
             if ( path === "root" ) {
-              m[obj.property] = JSON.parse( obj.newValue );
+              m[logEntry.property] = JSON.parse( logEntry.newValue );
             }
             else {
               var item = m;
@@ -112,10 +132,13 @@ define( function( require ) {
                 var pathElement = path[k];
                 item = item[pathElement];
               }
-              item[obj.property] = JSON.parse( obj.newValue );
+              item[logEntry.property] = JSON.parse( logEntry.newValue );
             }
 
             logIndex++;
+            if ( getLogEntry ) {
+              break;
+            }
           }
           else {
             break;
