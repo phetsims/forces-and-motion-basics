99bddfeb2167133a628338edf4ebe0661e55c916
Chris Malley <cmalley@pixelzoom.com>
2013-03-17 16:50:18 -0600

    add documentation, change return value name

--- contrib/locale-query.js
+++ contrib/locale-query.js
@@ -1,23 +1,36 @@
+// Copyright 2002-2013, University of Colorado
+
 /**
- * Read the query parameters from the browser address bar and parse as locale.
- * To be used in conjunction with the requirejs config.js file to specify language.  See forces-and-motion-basics for an example.
- * Author: Sam Reid, PhET
+ * Reads the query parameters from browser window's URL and parses as locale.
+ *
+ * 1. Use the i18n.js plugin for requirejs.
+ * 2. Add a reference to locale-query.js in your HTML file.
+ * 3. Add this code to your config.js:
+ *
+ *     config: {
+ *       i18n: {
+ *         locale: window.phet.getLocaleQuery()  //Specify the locale using a query parameter
+ *       }
+ *     }
+ * 4. Specify the locale in your URL like this: "http://path/to/my/sim.html?locale=fr
+ *
+ * @author: Sam Reid, PhET
  */
 if ( !window.phet ) {
   window.phet = {};
 }
 window.phet.getLocaleQuery = (function() {
-  var value = "en_US";
+  var locale = "en_US";
   if ( typeof window != 'undefined' && window.location.search ) {
     // look for first occurrence of "locale" query parameter
     var params = window.location.search.slice( 1 ).split( "&" );
     for ( var i = 0; i < params.length; i++ ) {
       var nameValuePair = params[i].split( "=" );
       if ( nameValuePair[0] === 'locale' ) {
-        value = decodeURI( nameValuePair[1] ).toLowerCase();
+        locale = decodeURI( nameValuePair[1] ).toLowerCase();
         break;
       }
     }
   }
-  return value;
+  return locale;
 });
