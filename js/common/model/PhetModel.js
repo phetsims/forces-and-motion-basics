07606badd410f6041eb707dcc9a7a39a251d4e62
Sam Reid <reids@colorado.edu>
2013-03-20 10:36:11 -0600

    added docs to PhetModel

--- js/common/model/PhetModel.js
+++ js/common/model/PhetModel.js
@@ -9,14 +9,14 @@ define( function( require ) {
   //TODO: Add automated tests
   var PhetModel = Backbone.Model.extend(
       {
-        //Add a listener and automatically call it back
+        //Add a listener and automatically call it back.  Unlike BackboneModel.on('change:x change:y') style, this only works for a single value
         sync: function( key, listener, thisRef ) {
           this.on( 'change:' + key, listener, thisRef );
           if ( typeof thisRef === "undefined" ) {listener( this, this[key] );}
           else {listener.call( thisRef, this, this[key] );} //Follow backbone pattern of passing the this instead of calling bind  
         },
 
-        //Get a property for one of the backbone model attributes
+        //Get a property interface for one of the backbone model attributes, which provides get/set/addListener methods
         property: function( key ) {
           var model = this;
           return {
@@ -25,10 +25,15 @@ define( function( require ) {
             addListener: function( listener ) { model.sync( key, listener ); } //TODO: should this be renamed to sync?
           };
         },
+
+        //When initialize is finished, call this method which will do the following:
+        // 1. create ES5 getters and setters for each property
+        // 2. store the initial state for reset
         initializeFinished: function() {
           var model = this;
           this.initialState = this.toJSON();//TODO: Stringify so it cannot be modified?
 
+          //Taken from https://gist.github.com/dandean/1292057, same as in github/Atlas
           function createProperty( name ) {
             Object.defineProperty( Object.getPrototypeOf( model ), name, {
               // Getter proxies to Model#get()...
@@ -50,7 +55,9 @@ define( function( require ) {
           }
         },
 
+        //Reset the model by setting the stored initial attributes
         //TODO: automatically detect attached child models that are PhetModel type and call reset on them too.
+        //TODO: if new values have been added, they may need to be cleared out
         reset: function() {
           this.set( this.initialState );
         }
