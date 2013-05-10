99a80b8d5761e337814499a59c347f7b654b2e0d
Sam Reid <reids@colorado.edu>
2013-05-10 09:33:34 -0600

    Load strings with update-locale.js

--- contrib/i18n.js
+++ contrib/i18n.js
@@ -37,27 +37,6 @@
 (function() {
   'use strict';
 
-  //PhET.SRR Added this code on 11/27/2012
-  //PhET.CM Modified this code on 11/28/2012
-  //Look for a query parameter of the form "locale=value", where the format of value is described in RFC 4646.
-  //The value of this parameter will be used to override the browser's language.
-  //Adapted from http://geekswithblogs.net/PhubarBaz/archive/2011/11/21/getting-query-parameters-in-javascript.aspx
-  var localeQueryParameter = (function() {
-    var value;
-    if ( typeof window != 'undefined' && window.location.search ) {
-      // look for first occurrence of "locale" query parameter
-      var params = window.location.search.slice( 1 ).split( "&" );
-      for ( var i = 0; i < params.length; i++ ) {
-        var nameValuePair = params[i].split( "=" );
-        if ( nameValuePair[0] === 'locale' ) {
-          value = decodeURI( nameValuePair[1] ).toLowerCase();
-          break;
-        }
-      }
-    }
-    return value;
-  }());
-
   //regexp for reconstructing the master bundle name from parts of the regexp match
   //nlsRegExp.exec("foo/bar/baz/nls/en-ca/foo") gives:
   //["foo/bar/baz/nls/en-ca/foo", "foo/bar/baz/nls/", "/", "/", "en-ca", "foo"]
@@ -148,12 +127,6 @@
                      (navigator.language ||
                       navigator.userLanguage || "root").toLowerCase();
           }
-          //PhET.CM added this block on 11/28/2012
-          //Override the browser's language using the optional query parameter.
-          if ( typeof localeQueryParameter === 'string' ) {
-            locale = localeQueryParameter;
-          }
-
           parts = locale.split( "-" );
         }
 
