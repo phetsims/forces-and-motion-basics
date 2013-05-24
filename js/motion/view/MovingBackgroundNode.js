2c2899c175e3d8846bd0e07abb7e4daab9ee75a9
Sam Reid <reids@colorado.edu>
2013-05-23 21:03:59 -0600

    more support for showing the gravel flecks, but commented out due to performance and rendering problems

--- js/motion/view/MovingBackgroundNode.js
+++ js/motion/view/MovingBackgroundNode.js
@@ -10,6 +10,7 @@ define( function( require ) {
   var ItemNode = require( 'motion/view/ItemNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var imageLoader = require( 'imageLoader' );
+  var linear = require( 'DOT/Util' ).linear;
 
   function MovingBackgroundNode( model, layoutCenterX ) {
     var movingBackgroundNode = this;
@@ -18,10 +19,9 @@ define( function( require ) {
 
     var modWidth = 120 * 15;
     var L = modWidth / 2;
-    var addBackgroundSprite = function( offset, imageName, distanceScale, y, scale ) {
-      var sprite = new Image( imageLoader.getImage( imageName ), {scale: scale, y: y} );
-      movingBackgroundNode.addChild( sprite );
-      var centering = layoutCenterX - sprite.width / 2;
+    var addBackgroundNode = function( offset, node, distanceScale ) {
+      movingBackgroundNode.addChild( node );
+      var centering = layoutCenterX - node.width / 2;
       model.link( 'position', function( position ) {
         var a = -position / distanceScale + offset;
         var n, z;
@@ -40,10 +40,10 @@ define( function( require ) {
           //n >= -(L+a)/2L
           n = Math.ceil( -(L + a) / 2 / L );
           z = n * 2 * L + a;
-          sprite.x = z + centering;
+          node.x = z + centering;
         }
         else if ( a < L ) {
-          sprite.x = a + centering;
+          node.x = a + centering;
         }
         else {
           //Put 'a' between -L and +L by subtracting an integral number of 2L
@@ -52,19 +52,23 @@ define( function( require ) {
           //n >= (L-a)/2L
           n = Math.floor( (L + a) / 2 / L );
           z = a - 2 * L * n;
-          sprite.x = z + centering;
+          node.x = z + centering;
         }
       } );
     };
+    var addBackgroundImage = function( offset, imageName, distanceScale, y, scale ) {
+      var sprite = new Image( imageLoader.getImage( imageName ), {scale: scale, y: y} );
+      addBackgroundNode( offset, sprite, distanceScale );
+    };
 
     var mountainY = 311;
-    addBackgroundSprite( L / 2, 'mountains.png', 10, mountainY, 1 );
-    addBackgroundSprite( L, 'mountains.png', 10, mountainY, 1 );
-    addBackgroundSprite( -L / 3, 'mountains.png', 10, mountainY, 1 );
+    addBackgroundImage( L / 2, 'mountains.png', 10, mountainY, 1 );
+    addBackgroundImage( L, 'mountains.png', 10, mountainY, 1 );
+    addBackgroundImage( -L / 3, 'mountains.png', 10, mountainY, 1 );
 
-    addBackgroundSprite( 100, 'cloud1.png', 5, 10, 1 );
-    addBackgroundSprite( 600, 'cloud1.png', 5, -30, 1 );
-    addBackgroundSprite( 1200, 'cloud1.png', 5, 5, 0.9 );
+    addBackgroundImage( 100, 'cloud1.png', 5, 10, 1 );
+    addBackgroundImage( 600, 'cloud1.png', 5, -30, 1 );
+    addBackgroundImage( 1200, 'cloud1.png', 5, 5, 0.9 );
 
     //We tested that Pattern has superior performance to a large cached image
     var tile = imageLoader.getImage( 'brick-tile.png' );
@@ -73,6 +77,47 @@ define( function( require ) {
     var offset = layoutCenterX - ground.width / 2;
     model.link( 'position', function( position ) { ground.x = -position % mod + offset; } );
     this.addChild( ground );
+
+    //Add the gravel and ice
+    if ( !model.skateboard && false ) {
+
+      movingBackgroundNode.lastNumSpecks = 0;
+      var gravelParent = new Node( {y: mountainY + 45} );
+      this.addChild( gravelParent );
+      addBackgroundNode( 0, gravelParent, 1 );
+
+      var updateGravelImage = function() {
+        var maxFriction = 2;
+        var width = 800;
+        var height = 5;
+        var numSpecks = linear( maxFriction * 0.1, 0, maxFriction, 500 * 1.15, model.friction );
+        numSpecks = numSpecks < 0 ? 0 : numSpecks;
+        console.log( numSpecks );
+
+        //Save computation, esp. for older machines
+        if ( numSpecks == movingBackgroundNode.lastNumSpecks ) {return;}
+
+        var node = new Node();
+        for ( var i = 0; i < numSpecks / 2; i++ ) {
+          node.addChild( new Rectangle( Math.floor( Math.random() * (width + 1) ), Math.floor( Math.random() * (height + 1) ), 1, 1, {fill: 'black'} ) );
+        }
+
+        for ( i = 0; i < numSpecks / 2; i++ ) {
+          node.addChild( new Rectangle( Math.floor( Math.random() * (width + 1) ), Math.floor( Math.random() * (height + 1) ), 1, 1, {fill: 'gray'} ) );
+        }
+
+        for ( i = 0; i < numSpecks / 10; i++ ) {
+          node.addChild( new Rectangle( Math.floor( Math.random() * (width + 1) ), Math.floor( Math.random() * (height + 1) ), 1, 1, {fill: 'white'} ) );
+        }
+        node.toImage( function( image ) {
+          gravelParent.children = [new Image( image )];
+        } );
+//        gravelParent.children = [node];
+//        movingBackgroundNode.addChild( node );
+        movingBackgroundNode.lastNumSpecks = numSpecks;
+      };
+      model.link( 'friction', updateGravelImage );
+    }
   }
 
   inherit( MovingBackgroundNode, Node );
