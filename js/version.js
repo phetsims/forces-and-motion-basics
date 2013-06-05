c6b58391c412d340aaf4fcf97bbd752658b170ba
Sam Reid <reids@colorado.edu>
2013-06-05 15:46:18 -0600

    updated version for deploy

--- js/version.js
+++ js/version.js
@@ -2,9 +2,9 @@
 
 /**
  * Returns version identifier for the simulation.
- * TODO: generate this file a build time, reading package.json
+ * TODO: generate this file at build time, reading package.json
  */
 define( function( require ) {
   'use strict';
-  return function() { return "0.0.0-17"; };
+  return function() { return "0.0.0-18"; };
 } );
