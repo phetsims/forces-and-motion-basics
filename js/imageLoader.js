63ac39ab329bb82d32e73e42c9c5e42ad2f33492
Chris Malley <cmalley@pixelzoom.com>
2013-06-07 12:55:00 -0600

    Move string filenames to imageNames property of image loader object, update FAMB to reflect this change. Move BLL, Molarity and example-sim to PxLoader and SimLauncher.

--- js/imageLoader.js
+++ js/imageLoader.js
@@ -4,7 +4,20 @@ define( function( require ) {
   //Will be added lazily after image loader complete.
   //Makes it possible to load through the module system rather than passed as parameter everywhere or used as global. 
 
-  var imageLoader = {};
-
-  return imageLoader;
+  return {
+    imageNames: 'Acceleration_Icon.png brick-tile.png cart.png cloud1.png crate.png ' +
+                'Friction_Icon.png fridge.png girl-holding.png girl-sitting.png girl-standing.png ' +
+                'go_hover.png go_pressed.png go_up.png grass.png ice_overlay.png man-holding.png ' +
+                'man-sitting.png man-standing.png Motion_icon.png mountains.png mystery-object-01.png ' +
+                'pull_figure_BLUE_0.png pull_figure_BLUE_3.png pull_figure_lrg_BLUE_0.png ' +
+                'pull_figure_lrg_BLUE_3.png pull_figure_lrg_RED_0.png pull_figure_lrg_RED_3.png ' +
+                'pull_figure_RED_0.png pull_figure_RED_3.png ' +
+                'pull_figure_small_BLUE_0.png   pull_figure_small_BLUE_3.png   pull_figure_small_RED_0.png   ' +
+                'pull_figure_small_RED_3.png   pusher_0.png   pusher_1.png ' +
+                'pusher_10.png pusher_11.png pusher_12.png pusher_13.png pusher_14.png pusher_2.png ' +
+                'pusher_3.png pusher_4.png pusher_5.png pusher_6.png pusher_7.png ' +
+                'pusher_8.png pusher_9.png pusher_fall_down.png pusher_straight_on.png ' +
+                'rope.png skateboard.png stop_hover.png stop_pressed.png stop_up.png trash-can.png ' +
+                'Tug_Icon.png water-bucket.png brick-repeat.svg handle_blue_top_grip_flat_gradient_3.svg'
+  };
 } );
