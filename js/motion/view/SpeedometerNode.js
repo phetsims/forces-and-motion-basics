5f71b409959f0197453e71f23d5007a8ecddaf1d
samreid <reids@colorado.edu>
2013-07-16 11:14:22 -0600

    minor cleanup and added comments

--- js/motion/view/SpeedometerNode.js
+++ js/motion/view/SpeedometerNode.js
@@ -1,5 +1,10 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
+/**
+ * The speedometer node is a scenery node view that shows the speed of the moving object(s).
+ *
+ * @author Sam Reid
+ */
 define( function( require ) {
   'use strict';
 
@@ -13,12 +18,18 @@ define( function( require ) {
   var MotionConstants = require( 'motion/MotionConstants' );
   var FAMBFont = require( 'common/view/FAMBFont' );
 
+  //Constants
   var ANGLE_PER_TICK = Math.PI * 2 / 4 / 8;
   var NUM_TICKS = ( 8 + 2 ) * 2 + 1;
 
+  /**
+   * Constructor
+   * @param {Property} velocityProperty Property<Number> for the velocity, which is converted to speed for the speedometer
+   * @param {Object} options typical Node layout and display options
+   * @constructor
+   */
   function SpeedometerNode( velocityProperty, options ) {
-    options = _.extend( {renderer: 'svg'}, options );
-    Node.call( this, options );
+    Node.call( this, _.extend( {renderer: 'svg'}, options ) );
     var radius = 67;
     this.addChild( new Circle( radius, {fill: 'white', stroke: '#555555', lineWidth: 2} ) );
 
@@ -35,14 +46,15 @@ define( function( require ) {
     var startAngle = -1 / 2 * Math.PI - totalAngle / 2;
     var endAngle = startAngle + totalAngle;
 
+    //Update when the velocity changes
     velocityProperty.link( function( velocity ) {
       needle.resetTransform();
 
-      //TODO: factor out max speed constant
       var needleAngle = linear( 0, MotionConstants.maxSpeed, startAngle, endAngle, Math.abs( velocity ) );
       needle.rotateAround( {x: 0, y: 0}, needleAngle );
     } );
 
+    //Add the tick marks
     for ( var i = 0; i < NUM_TICKS; i++ ) {
       var tickAngle = i * ANGLE_PER_TICK + startAngle;
       var tickLength = i % 2 === 0 ? 10 : 5;
@@ -52,7 +64,5 @@ define( function( require ) {
     }
   }
 
-  inherit( Node, SpeedometerNode );
-
-  return SpeedometerNode;
+  return inherit( Node, SpeedometerNode );
 } );
