5900c21e442e3842647e6640a808a1eb1e318a47
samreid <reids@colorado.edu>
2013-07-16 11:16:43 -0600

    minor cleanup and added comments

--- js/motion/MotionConstants.js
+++ js/motion/MotionConstants.js
@@ -13,8 +13,10 @@ define( function() {
     //The scale mapping between model units (meters) and stage coordinates, How much to translate model coordinates into view pixels for translating the background ground
     positionScale: 40,
 
+    //Coefficient of friction (Friction is unitless and not bounded between 0 and 1)
     maxFriction: 0.5,
 
+    //Maximum speed in meters/second before the pusher falls down
     maxSpeed: 20
   };
 } );
