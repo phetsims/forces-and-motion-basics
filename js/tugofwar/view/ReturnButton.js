8964cf29be92715be285cf927d8087c18c23307f
Chris Malley <cmalley@pixelzoom.com>
2013-07-22 13:36:22 -0600

    add visual state feedback to RectangleButton, fix call sites to use 'rectangleFillUp' instead of 'rectangleFill', and to use Color instead of String for specifying the color (so that colors for other states can be computed)

--- js/tugofwar/view/ReturnButton.js
+++ js/tugofwar/view/ReturnButton.js
@@ -13,6 +13,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var TextButton = require( 'SUN/TextButton' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Color = require( 'SCENERY/util/Color' );
 
   /**
    * @param {TugOfWarModel} model
@@ -22,7 +23,8 @@ define( function( require ) {
   function ReturnButton( model, options ) {
     Node.call( this );
 
-    var button = new TextButton( 'Return', model.returnCart.bind( model ), {font: new PhetFont( 16, 'bold' ), rectangleFill: '#ffd438'} );
+    var button = new TextButton( 'Return', model.returnCart.bind( model ),
+      {font: new PhetFont( 16, 'bold' ), rectangleFillUp: new Color( 254, 192, 0 )} );
     this.addChild( button );
     this.mutate( options );
 
