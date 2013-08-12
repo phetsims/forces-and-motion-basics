f151540abb695a39ba0d4ffe8506c98a7994b798
samreid <reids@colorado.edu>
2013-08-12 10:27:02 -0600

    Made friction slider more symmetrical in its layout and added a workaround for the text bounds problem

--- js/motion/view/MotionControlPanel.js
+++ js/motion/view/MotionControlPanel.js
@@ -59,7 +59,7 @@ define( function( require ) {
     var spacer2 = function() { return new Rectangle( 0, 0, 10, 10, {visible: false} ); };
 
     var createFrictionSlider = function() {
-      var createTick = function( label ) {
+      var createTick = function( label, visible ) {
         var path = new Path( {shape: Shape.lineSegment( new Vector2( 0, 0 ), new Vector2( 0, -18 ) ), stroke: 'black', lineWidth: 1} );
         var text = new Text( label, {font: new PhetFont( 15 )} );
         model.stack.lengthProperty.link( function( length ) {
@@ -67,17 +67,33 @@ define( function( require ) {
           path.fill = enabled ? 'black' : 'gray';
           text.fill = enabled ? 'black' : 'gray';
         } );
-        return new VBox( {children: [ text, path ]} );
+        return new VBox( {children: [ text, path ], pickable: false, visible: visible} );
       };
 
-      var frictionSlider = new HSlider( 0, MotionConstants.MAX_FRICTION, 150, model.frictionProperty, new Property( 'WITHIN_ALLOWED_RANGE' ), null, null, {zeroOnRelease: false} ).addTick( 0, createTick( 'None' ) ).addTick( 1, createTick( 'Lots' ) );
+      //Create the friction slider and its labels.
+      // Add invisible symmetric ticks + labels so the slider will be perfectly centered.  A better way to do this would be just to line things up based on the track of the slider,
+      // but this makes it work with VBox/HBox
+      var frictionSlider = new HSlider( 0, MotionConstants.MAX_FRICTION, 150, model.frictionProperty, new Property( 'WITHIN_ALLOWED_RANGE' ), null, null, {zeroOnRelease: false} ).
+        addTick( 0, createTick( 'None', true ) ).addTick( 1, createTick( 'Lots', true ) ).
+        addTick( 0, createTick( 'Lots', false ) ).addTick( 1, createTick( 'None', false ) );
       var frictionLabel = new Text( 'Friction', new PhetFont( fontSize, 'bold' ) );
-      return new VBox( {spacing: -8, children: [new HBox( {children: [ frictionLabel, spacer2() ]} ), frictionSlider]} );
+      frictionLabel.font = new PhetFont( fontSize + 1, 'bold' );
+      frictionLabel.renderer = 'svg';
+
+      //Workaround for a scenery bug that has the wrong bounds with the hybrid approach.
+      frictionLabel.boundsMethod = 'fast';
+      return new VBox( {spacing: -8, children: [frictionLabel , frictionSlider]} );
     };
+
+    //For debugging, wrap a node in a rectangle showing its bounds and return it
+    function wrap( node ) {
+      node.addChild( new Rectangle( node.bounds.minX, node.bounds.minY, node.bounds.maxX - node.bounds.minX, node.bounds.maxY - node.bounds.minY, {stroke: 'blue', lineWidth: 1} ) );
+      return node;
+    }
 
     var indent = 24;
     var controlPanel = new VBox( {
-      align: 'left',
+      align: 'center',
       children: model.tab === 'motion' ?
                 [ new VerticalCheckBoxGroup(
                   [
@@ -94,7 +110,7 @@ define( function( require ) {
                     toElement( Strings.values, 'showValues', {indent: indent} ),
                     toElement( Strings.masses, 'showMasses' ),
                     toElement( Strings.speed, 'showSpeed', {icon: speedometerIcon()} )
-                  ], {fill: '#e3e980'} ), spacer(), new HBox( {children: [spacer2(), createFrictionSlider()]} )] :
+                  ], {fill: '#e3e980'} ), spacer(), createFrictionSlider() ] :
                 [ new VerticalCheckBoxGroup(
                   [
                     toElement( Strings.forces, 'showForce', {icon: arrowIcon()} ),
@@ -103,7 +119,7 @@ define( function( require ) {
                     toElement( Strings.masses, 'showMasses' ),
                     toElement( Strings.speed, 'showSpeed', {icon: speedometerIcon()} ),
                     toElement( Strings.acceleration, 'showAcceleration', {icon: accelerometerIcon()} )
-                  ], {fill: '#e3e980'} ), spacer(), new HBox( {children: [spacer2(), createFrictionSlider()]} )]
+                  ], {fill: '#e3e980'} ), spacer(), createFrictionSlider() ]
     } );
     var panelNode = new Panel( controlPanel, {fill: '#e3e980'} );
     this.addChild( panelNode.mutate( { left: 981 - panelNode.width - 5, top: 5} ) );
