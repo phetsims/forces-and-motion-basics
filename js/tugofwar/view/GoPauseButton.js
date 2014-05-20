7728c381f957677a92e3dc9eee53cf4d1f3165a0
Sam Reid <reids@colorado.edu>
2014-05-20 15:50:38 -0600

    Updated GoPauseButton to use the latest sun buttons

--- js/tugofwar/view/GoPauseButton.js
+++ js/tugofwar/view/GoPauseButton.js
@@ -13,16 +13,33 @@ define( function( require ) {
   var Text = require( 'SCENERY/nodes/Text' );
   var inherit = require( 'PHET_CORE/inherit' );
   var goString = require( 'string!FORCES_AND_MOTION_BASICS/go' );
-  var stopUpImage = require( 'image!FORCES_AND_MOTION_BASICS/stop_up.png' );
   var pauseString = require( 'string!FORCES_AND_MOTION_BASICS/pause' );
-  var goUpImage = require( 'image!FORCES_AND_MOTION_BASICS/go_up.png' );
-  var goHoverImage = require( 'image!FORCES_AND_MOTION_BASICS/go_hover.png' );
-  var stopHoverImage = require( 'image!FORCES_AND_MOTION_BASICS/stop_hover.png' );
-  var stopPressedImage = require( 'image!FORCES_AND_MOTION_BASICS/stop_pressed.png' );
-  var goPressedImage = require( 'image!FORCES_AND_MOTION_BASICS/go_pressed.png' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var PushButtonDeprecated = require( 'SUN/PushButtonDeprecated' );
+  var RoundPushButton = require( 'SUN/buttons/RoundPushButton' );
   var ToggleNode = require( 'SUN/ToggleNode' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+
+  //Given nodes that have possibly different sizes, wrap the specified node in a parent empty Rectangle node so the bounds will match up
+  //If the node is already the largest, don't wrap it.
+  //Centers all the nodes in the parent wrappers
+  //TODO: Would be good to factor this out or provide better library support
+  function wrap( node, padX, padY, nodes ) {
+    var maxWidth = -1;
+    var maxHeight = -1;
+    nodes.forEach( function( n ) {
+      if ( n.width > maxWidth ) {
+        maxWidth = n.width;
+      }
+      if ( n.height > maxHeight ) {
+        maxHeight = n.height;
+      }
+    } );
+    maxWidth += padX;
+    maxHeight += padY;
+    node.centerX = maxWidth / 2;
+    node.centerY = maxHeight / 2;
+    return new Rectangle( 0, 0, maxWidth, maxHeight, {children: [node]} );
+  }
 
   /**
    * Create a GoPauseButton that appears below the candy cart when a puller has been attached to the rope.
@@ -33,47 +50,25 @@ define( function( require ) {
   function GoPauseButton( model, layoutWidth ) {
     var goPauseButton = this;
 
-    //Pre create the text icons because dynamically changing text currently 4-1-2013 looks buggy on iPad 3
-    var textOptions = {font: new PhetFont( 30 )};
-    var textWidth = goUpImage.width - 30;//Trim the edges because of the shadow and button padding
+    var padX = 15;
+    var padY = 10;
+    var goText = new Text( goString, {font: new PhetFont( 42 )} );
+    var pauseText = new Text( pauseString, {font: new PhetFont( 30 )} );
 
-    var goText = new Text( goString, textOptions );
-    if ( goText.width > textWidth ) { goText.scale( textWidth / goText.width ); }
-
-    var pauseText = new Text( pauseString, textOptions );
-    if ( pauseText.width > textWidth ) { pauseText.scale( textWidth / pauseText.width ); }
-
-    var goButton = new PushButtonDeprecated( new Image( goUpImage ), new Image( goHoverImage ), new Image( goPressedImage ), new Image( goUpImage ) );
-
-    //Account for the button not being centered
-    goText.center = goButton.center.plusXY( -3, -5 );
-    goButton.addChild( goText );
-
-    goButton.addListener( function() { model.running = true; } );
-
-    var pauseButton = new PushButtonDeprecated( new Image( stopUpImage ), new Image( stopHoverImage ), new Image( stopPressedImage ), new Image( stopUpImage ) );
-
-    //Account for the button not being centered
-    pauseText.center = pauseButton.center.plusXY( -3, -5 );
-    pauseButton.addChild( pauseText );
-
-    pauseButton.addListener( function() { model.running = false; } );
+    var goButton = new RoundPushButton( {content: wrap( goText, padX, padY, [goText, pauseText] ), baseColor: '#94b830', listener: function() {model.running = true;}} );//green
+    var pauseButton = new RoundPushButton( {content: wrap( pauseText, padX, padY, [goText, pauseText] ), baseColor: '#df1a22', listener: function() {model.running = false;}} );//red
 
     var showGoButtonProperty = model.toDerivedProperty( ['running', 'state', 'numberPullersAttached'], function( running, state, numberPullersAttached ) {
       return !running;
     } );
-    ToggleNode.call( this, goButton, pauseButton, showGoButtonProperty, {y: 400} );
+    ToggleNode.call( this, goButton, pauseButton, showGoButtonProperty, {top: 400} );
 
     //Show the go/pause button if any pullers are attached or if the cart got started moving, and if it hasn't already finished a match, see #61
-    var showGoPauseButtonProperty = model.toDerivedProperty( ['running', 'state', 'numberPullersAttached'], function( running, state, numberPullersAttached ) {
+    model.toDerivedProperty( ['running', 'state', 'numberPullersAttached'], function( running, state, numberPullersAttached ) {
       goPauseButton.visible = state !== 'completed' && (numberPullersAttached > 0 || running);
-    } );
-    showGoPauseButtonProperty.linkAttribute( this, 'visible' );
+    } ).linkAttribute( this, 'visible' );
 
     this.centerX = layoutWidth / 2;
-
-    //Add accessibility peer
-    this.addPeer( '<input type="button">', {click: function() {model.running = !model.running;}} );
   }
 
   return inherit( ToggleNode, GoPauseButton );
