8655eff2837e7e15e8d9dd79e967cdb0f9579dd3
Chris Malley <cmalley@pixelzoom.com>
2015-02-11 18:39:27 -0700

    move requirejs plugins to chipper/js/requirejs-plugins, phetsims/chipper#92

--- js/forces-and-motion-basics-config.js
+++ js/forces-and-motion-basics-config.js
@@ -21,9 +21,9 @@ require.config( {
     SHERPA: '../../sherpa/',
 
     //Load plugins
-    image: '../../chipper/requirejs-plugins/image',
-    audio: '../../chipper/requirejs-plugins/audio',
-    string: '../../chipper/requirejs-plugins/string',
+    image: '../../chipper/js/requirejs-plugins/image',
+    audio: '../../chipper/js/requirejs-plugins/audio',
+    string: '../../chipper/js/requirejs-plugins/string',
 
     text: '../../sherpa/text'
   },
