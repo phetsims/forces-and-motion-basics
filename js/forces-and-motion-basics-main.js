fc27e7842ee59d346072e96760261fdbf73f34c9
Sam Reid <reids@colorado.edu>
2015-02-18 11:08:45 -0700

    Added initial text for when the sim launches

--- js/forces-and-motion-basics-main.js
+++ js/forces-and-motion-basics-main.js
@@ -31,7 +31,13 @@ define( function( require ) {
       leadDesign: 'Noah Podolefsky',
       softwareDevelopment: 'Sam Reid',
       team: 'Trish Loeblein, Ariel Paul, Kathy Perkins'
-    }
+    },
+    textDescription: '. There is a heavily loaded cart on wheels sitting on a track. ' +
+                     'Attached to the left side of the cart is a thick 8 foot rope with 4 large knots spaced ' +
+                     'at equal intervals. Standing near this knotted rope is a group of 4 people. On the opposite ' +
+                     'side of the cart, a similar rope with 4 large knots is attached to the right side of the cart. ' +
+                     'There is another group of 4 people - they are standing near this other rope. ' +
+                     'The centre position of the cart has been marked on the ground.'
   };
 
   SimLauncher.launch( function() {
