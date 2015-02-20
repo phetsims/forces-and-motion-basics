50c68555941d1ef86a2c4d87f51f66240c7b42be
Sam Reid <reids@colorado.edu>
2015-02-19 22:43:29 -0700

    Factored out ForcesAndMotionBasicsSim

---
+++ js/ForcesAndMotionBasicsSim.js
@@ -0,0 +1,57 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ *
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Sim = require( 'JOIST/Sim' );
+  var TugIcon = require( 'image!FORCES_AND_MOTION_BASICS/Tug_Icon.png' );
+  var MotionIcon = require( 'image!FORCES_AND_MOTION_BASICS/Motion_Icon.png' );
+  var FrictionIcon = require( 'image!FORCES_AND_MOTION_BASICS/Friction_Icon.png' );
+  var AccelerationIcon = require( 'image!FORCES_AND_MOTION_BASICS/Acceleration_Icon.png' );
+  var titleString = require( 'string!FORCES_AND_MOTION_BASICS/forces-and-motion-basics.name' );
+  var netForceString = require( 'string!FORCES_AND_MOTION_BASICS/netForce' );
+  var motionString = require( 'string!FORCES_AND_MOTION_BASICS/motion' );
+  var frictionString = require( 'string!FORCES_AND_MOTION_BASICS/friction' );
+  var accelerationString = require( 'string!FORCES_AND_MOTION_BASICS/acceleration' );
+  var NetForceModel = require( 'FORCES_AND_MOTION_BASICS/netforce/model/NetForceModel' );
+  var NetForceScreenView = require( 'FORCES_AND_MOTION_BASICS/netforce/view/NetForceScreenView' );
+  var Image = require( 'SCENERY/nodes/Image' );
+  var Screen = require( 'JOIST/Screen' );
+  var MotionScreen = require( 'FORCES_AND_MOTION_BASICS/motion/MotionScreen' );
+
+  /**
+   *
+   * @constructor
+   */
+  function ForcesAndMotionBasicsSim( options ) {
+
+    //Provide the screen names as named fields so they can be easily accessed dynamically, for API features
+    //And lookups will still work properly even if the screens are reduced with ?screens=...
+    this.netForceScreen = new Screen( netForceString, new Image( TugIcon ),
+      function() {return new NetForceModel();},
+      function( model ) {return new NetForceScreenView( model );}
+    );
+    this.motionScreen = new MotionScreen( motionString, new Image( MotionIcon ), 'motion' );
+    this.frictionScreen = new MotionScreen( frictionString, new Image( FrictionIcon ), 'friction' );
+    this.accelerationScreen = new MotionScreen( accelerationString, new Image( AccelerationIcon ), 'acceleration' );
+
+    //Create and start the sim
+    Sim.call( this,
+      titleString, [
+        this.netForceScreen,
+        this.motionScreen,
+        this.frictionScreen,
+        this.accelerationScreen
+      ],
+      options );
+  }
+
+  return inherit( Sim, ForcesAndMotionBasicsSim );
+} );
--- js/forces-and-motion-basics-main.js
+++ js/forces-and-motion-basics-main.js
@@ -8,23 +8,8 @@
 define( function( require ) {
   'use strict';
 
-  var
-    NetForceModel = require( 'FORCES_AND_MOTION_BASICS/netforce/model/NetForceModel' ),
-    NetForceScreenView = require( 'FORCES_AND_MOTION_BASICS/netforce/view/NetForceScreenView' ),
-    Image = require( 'SCENERY/nodes/Image' ),
-    Screen = require( 'JOIST/Screen' ),
-    Sim = require( 'JOIST/Sim' ),
-    SimLauncher = require( 'JOIST/SimLauncher' ),
-    TugIcon = require( 'image!FORCES_AND_MOTION_BASICS/Tug_Icon.png' ),
-    MotionIcon = require( 'image!FORCES_AND_MOTION_BASICS/Motion_Icon.png' ),
-    FrictionIcon = require( 'image!FORCES_AND_MOTION_BASICS/Friction_Icon.png' ),
-    AccelerationIcon = require( 'image!FORCES_AND_MOTION_BASICS/Acceleration_Icon.png' ),
-    titleString = require( 'string!FORCES_AND_MOTION_BASICS/forces-and-motion-basics.name' ),
-    netForceString = require( 'string!FORCES_AND_MOTION_BASICS/netForce' ),
-    motionString = require( 'string!FORCES_AND_MOTION_BASICS/motion' ),
-    frictionString = require( 'string!FORCES_AND_MOTION_BASICS/friction' ),
-    accelerationString = require( 'string!FORCES_AND_MOTION_BASICS/acceleration' ),
-    MotionScreen = require( 'FORCES_AND_MOTION_BASICS/motion/MotionScreen' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+  var ForcesAndMotionBasicsSim = require( 'FORCES_AND_MOTION_BASICS/ForcesAndMotionBasicsSim' );
 
   var simOptions = {
     credits: {
@@ -41,16 +26,6 @@ define( function( require ) {
   };
 
   SimLauncher.launch( function() {
-
-    //Create and start the sim
-    new Sim( titleString, [
-      new Screen( netForceString, new Image( TugIcon ),
-        function() {return new NetForceModel();},
-        function( model ) {return new NetForceScreenView( model );}
-      ),
-      new MotionScreen( motionString, new Image( MotionIcon ), 'motion' ),
-      new MotionScreen( frictionString, new Image( FrictionIcon ), 'friction' ),
-      new MotionScreen( accelerationString, new Image( AccelerationIcon ), 'acceleration' )
-    ], simOptions ).start();
+    new ForcesAndMotionBasicsSim( simOptions ).start();
   } );
 } );
