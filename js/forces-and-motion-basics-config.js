aa97a2628ec8cb36d0f28d1a66a11d87ce9ad350
Chris Malley <cmalley@pixelzoom.com>
2015-05-15 16:51:13 -0600

    move all 3rd-party libs to sherpa/lib/, phetsims/sherpa#17

--- js/forces-and-motion-basics-config.js
+++ js/forces-and-motion-basics-config.js
@@ -19,14 +19,15 @@ require.config( {
     JOIST: '../../joist/js',
     FORCES_AND_MOTION_BASICS: '../../forces-and-motion-basics/js',
     VIBE: '../../vibe/js',
-    SHERPA: '../../sherpa/',
+    SHERPA: '../../sherpa/lib',
 
     //Load plugins
     image: '../../chipper/js/requirejs-plugins/image',
     audio: '../../chipper/js/requirejs-plugins/audio',
     string: '../../chipper/js/requirejs-plugins/string',
 
-    text: '../../sherpa/text-2.0.12'
+    // third-party libs
+    text: '../../sherpa/lib/text-2.0.12'
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
