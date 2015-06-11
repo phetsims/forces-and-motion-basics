d4c952a42d058bdf11297d61c72ce6b6c00833b0
Sam Reid <reids@colorado.edu>
2015-06-11 14:02:23 -0600

    Removed unnecessary TODO

--- js/common/view/ForcesAndMotionBasicsLayoutBounds.js
+++ js/common/view/ForcesAndMotionBasicsLayoutBounds.js
@@ -6,7 +6,6 @@
  * The aspect ratio that this sim was coded for differs by 7% than the one we eventually decided upon.
  * aspect ratio of this screen: 981/604=1.62
  * aspect ratio for default: 768/504=1.52
- * TODO: Rewrite the sim layout to use the standard bounds (lower priority)?
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
