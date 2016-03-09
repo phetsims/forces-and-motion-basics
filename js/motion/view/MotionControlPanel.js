eab5af8571150134768b616660bd06dd6f77c9b8
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-03-08 19:29:50 -0700

    Rewrite control panel to support disabled check boxes, see #166

--- js/motion/view/MotionControlPanel.js
+++ js/motion/view/MotionControlPanel.js
@@ -12,7 +12,10 @@ define( function( require ) {
   var Text = require( 'SCENERY/nodes/Text' );
   var VBox = require( 'SCENERY/nodes/VBox' );
   var HBox = require( 'SCENERY/nodes/HBox' );
+  var Property = require( 'AXON/Property' );
+  var CheckBox = require( 'SUN/CheckBox' );
   var VStrut = require( 'SCENERY/nodes/VStrut' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
   var SliderKnob = require( 'FORCES_AND_MOTION_BASICS/common/view/SliderKnob' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var HSlider = require( 'SUN/HSlider' );
@@ -28,7 +31,6 @@ define( function( require ) {
   var noneString = require( 'string!FORCES_AND_MOTION_BASICS/none' );
   var Panel = require( 'SUN/Panel' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var VerticalCheckBoxGroup = require( 'SUN/VerticalCheckBoxGroup' );
   var MotionConstants = require( 'FORCES_AND_MOTION_BASICS/motion/MotionConstants' );
   var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
   var GaugeNode = require( 'SCENERY_PHET/GaugeNode' );
@@ -48,15 +50,50 @@ define( function( require ) {
     var fontSize = 18;
     var maxTextWidth = 250;
 
-    var toElement = function( text, propertyName, options ) {
-      options = _.extend( { indent: 0 }, options );
-      var textNode = new Text( text, { font: new PhetFont( fontSize ), maxWidth: maxTextWidth } );
-      return {
-        //TODO: Why is this immense spacing necessary here?
-        content: options.icon ? new HBox( { spacing: 10, children: [ textNode, options.icon ] } ) : textNode,
-        property: model.property( propertyName ),
-        indent: options.indent
-      };
+    /**
+     * Create a check box that will be placed in this control panel with others in a VBox.
+     * Has optional horizontal indentation, icon, and property to control how the checkbox is enabled.
+     *
+     * @param {string} text - label for the check box
+     * @param {Property<boolean>} propertyName
+     * @param {object} options
+     */
+    var createCheckBox = function( text, propertyName, options ) {
+      options = _.extend( { 
+        indent: 0,
+        checkBoxEnabledProperty: new Property( true ),
+        icon: null
+      }, options );
+
+      // container for the checkbox and optional indentation and icon
+      var checkBoxContainer = new HBox( { spacing: 0 } );
+
+      // create the horizontal spacer for the indentation and add it to the container
+      var hSeparator = new HStrut( options.indent );
+      checkBoxContainer.insertChild( 0, hSeparator );
+
+      // create the label for the checkbox
+      var labelText = new Text( text, { font: new PhetFont( fontSize ), maxWidth: maxTextWidth } );
+
+      // create the check box and insert it into the container
+      var checkBox = new CheckBox( labelText, model.property( propertyName ) );
+      checkBoxContainer.insertChild( 1, checkBox );
+
+      // add optional icon next to checkbox
+      if( options.icon ) {
+        // create a horizontal spacer for the icon
+        var iconSpacer = new HStrut( 10 );
+        checkBoxContainer.insertChild( 2, iconSpacer );
+        checkBoxContainer.insertChild( 3, options.icon );
+      }
+
+      // link the property to the enabled state of the check box
+      // check box persists for the lifetime of the simulation, no dispose necessary
+      options.checkBoxEnabledProperty.link( function( enabled ) {
+        checkBox.enabled = enabled;
+      } );
+
+      return checkBoxContainer;
     };
 
     //Icon for the forces in the control panel
@@ -104,43 +141,52 @@ define( function( require ) {
 
     // Create controls for the 'motion' screen
     var createMotionControls = function() {
-      return [ new VerticalCheckBoxGroup( [
-        toElement( forceString, 'showForce', { icon: arrowIcon() } ),
-        toElement( valuesString, 'showValues' ),
-        toElement( massesString, 'showMasses' ),
-        toElement( speedString, 'showSpeed', { icon: speedometerIcon() } )
-      ], { fill: '#e3e980' } ) ];
+      var vBox = new VBox( {
+          children: [
+            createCheckBox( forceString, 'showForce', { icon: arrowIcon() } ),
+            createCheckBox( valuesString, 'showValues' ),
+            createCheckBox( massesString, 'showMasses' ),
+            createCheckBox( speedString, 'showSpeed', {icon: speedometerIcon() } )
+          ],
+          align: 'left',
+          spacing: 10
+        } );
+      return [ vBox ];
     };
 
     // Create controls for the 'friction' screen
     var createFrictionControls = function() {
-      return [
-        new VerticalCheckBoxGroup( [
-          toElement( forcesString, 'showForce', { icon: arrowIcon() } ),
-          toElement( sumOfForcesString, 'showSumOfForces', { indent: indent } ),
-          toElement( valuesString, 'showValues' ),
-          toElement( massesString, 'showMasses' ),
-          toElement( speedString, 'showSpeed', { icon: speedometerIcon() } )
-        ], { fill: '#e3e980' } ),
-        new VStrut( 12 ),
-        createFrictionSlider() ];
+      var vBox = new VBox( {
+        children: [
+          createCheckBox( forcesString, 'showForce', { icon: arrowIcon() } ),
+          createCheckBox( sumOfForcesString, 'showSumOfForces', { indent: indent } ),
+          createCheckBox( valuesString, 'showValues' ),
+          createCheckBox( massesString, 'showMasses' ),
+          createCheckBox( speedString, 'showSpeed', { icon: speedometerIcon() } )
+        ],
+        align: 'left',
+        spacing: 10
+      } );
+      return [ vBox, new VStrut( 12 ), createFrictionSlider() ];
     };
 
     // Create controls for the 'acceleration' screen
     var createAccelerationControls = function() {
-      return [
-        new VerticalCheckBoxGroup( [
-          toElement( forcesString, 'showForce', { icon: arrowIcon() } ),
-          toElement( sumOfForcesString, 'showSumOfForces', { indent: indent } ),
-          toElement( valuesString, 'showValues' ),
-          toElement( massesString, 'showMasses' ),
-          toElement( speedString, 'showSpeed', { icon: speedometerIcon() } ),
-          toElement( accelerationString, 'showAcceleration', { icon: accelerometerIcon() } )
-        ], { fill: '#e3e980' } ),
-        new VStrut( 12 ),
-        createFrictionSlider()
-      ];
+      var vBox = new VBox( {
+        children: [
+          createCheckBox( forcesString, 'showForce', { icon: arrowIcon() } ),
+          createCheckBox( sumOfForcesString, 'showSumOfForces', { indent: indent } ),
+          createCheckBox( valuesString, 'showValues' ),
+          createCheckBox( massesString, 'showMasses' ),
+          createCheckBox( speedString, 'showSpeed', { icon: speedometerIcon() } ),
+          createCheckBox( accelerationString, 'showAcceleration', { icon: accelerometerIcon() } )
+        ],
+        align: 'left',
+        spacing: 10
+      } );
+      return [ vBox, new VStrut( 12 ), createFrictionSlider() ];
     };
+
 
     var controlPanel = new VBox( {
       align: 'center',
@@ -148,7 +194,7 @@ define( function( require ) {
                 model.screen === 'friction' ? createFrictionControls() :
                 createAccelerationControls()
     } );
-    var panelNode = new Panel( controlPanel, { xMargin: 10, yMargin: 7, fill: '#e3e980' } );
+    var panelNode = new Panel( controlPanel, { xMargin: 12, yMargin: 7, fill: '#e3e980' } );
     this.addChild( panelNode.mutate( { left: 981 - panelNode.width - 5, top: 5 } ) );
   }
 
