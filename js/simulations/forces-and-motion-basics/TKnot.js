76c322bc764e61d32b15618ca71f069431f4dda7
samreid <reids@colorado.edu>
2016-07-22 10:35:49 -0600

    Instrumented PropertySet instances, see https://github.com/phetsims/phet-io/issues/502

---
+++ js/simulations/forces-and-motion-basics/TKnot.js
@@ -0,0 +1,44 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var TObject = require( 'PHET_IO/types/TObject' );
+
+  var TKnot = phetioInherit( TObject, 'TKnot', function( instance, phetioID ) {
+    TObject.call( this, instance, phetioID );
+    assert && assert( !!instance, 'instance should exist' );
+    assertInstanceOf( instance, phet.forcesAndMotionBasics.Knot );
+  }, {}, {
+    fromStateObject: function( stateObject ) {
+      if ( stateObject === 'null' ) {
+        return null;
+      }
+      else {
+        return phetio.getWrapper( stateObject ).instance;
+      }
+    },
+    toStateObject: function( instance ) {
+      if ( instance ) {
+        return instance.phetioID;
+      }
+      else {
+        return 'null';
+      }
+    }
+  } );
+
+  phetioNamespace.register( 'TKnot', TKnot );
+
+  return TKnot;
+} );
+
