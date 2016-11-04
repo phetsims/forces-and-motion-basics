f5994f54a4e91998c5f50d9e1a39f6f10b14edeb
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-11-04 16:35:59 -0600

    center labels when items are sent home, see #207

--- js/motion/view/ItemNode.js
+++ js/motion/view/ItemNode.js
@@ -177,7 +177,9 @@ define( function( require ) {
           }
         }
         else {
+          // send the item home and make sure that the label is centered
           item.animateHome();
+          self.labelNode.centerX = normalImageNode.centerX;
         }
       }
     } );
