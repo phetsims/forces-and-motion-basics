983353f7b9822914ecfb6758f88571eed7c9d323
samreid <reids@colorado.edu>
2016-12-13 21:20:09 -0700

    Added newly required instrumentation

---
+++ js/simulations/forces-and-motion-basics/TItem.js
@@ -0,0 +1,34 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var TObject = require( 'PHET_IO/types/TObject' );
+  var phetio = require( 'PHET_IO/phetio' );
+
+  var TItem = function( instance, phetioID ) {
+    TObject.call( this, instance, phetioID );
+    assert && assert( !!instance, 'instance should exist' );
+    assertInstanceOf( instance, phet.forcesAndMotionBasics.Item );
+  };
+
+  phetioInherit( TObject, 'TItem', TItem, {}, {
+
+    toStateObject: function( instance ) {
+      return instance.name;
+    }
+  } );
+
+  phetioNamespace.register( 'TItem', TItem );
+
+  return TItem;
+} );
+
