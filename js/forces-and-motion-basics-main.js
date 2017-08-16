b78d6246931e5255b0a32d67e9e818f101cbf5d2
zepumph <michael.kauzmann@colorado.edu>
2017-08-15 16:39:03 -0800

    remove unnecessary newlines or replace with <br>, https://github.com/phetsims/joist/issues/435

--- js/forces-and-motion-basics-main.js
+++ js/forces-and-motion-basics-main.js
@@ -41,7 +41,8 @@ define( function( require ) {
       graphicArts: 'Mariah Hermsmeyer, Sharon Siman-Tov',
       softwareDevelopment: 'Jesse Greenberg, Sam Reid',
       team: 'Amy Rouinfar, Trish Loeblein, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, \n\tElise Morgan, Oliver Orejola, Ben Roberts, Bryan Yoelin'
+      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Elise Morgan, Oliver Orejola, Ben Roberts, ' +
+                        'Bryan Yoelin'
     }
   };
 
