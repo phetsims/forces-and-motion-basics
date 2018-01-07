658b6fa312a1f85d269345423f85fa0018ec3f8a
samreid <reids@colorado.edu>
2018-01-07 08:19:42 -0700

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/643

--- js/netforce/view/NetForceControlPanel.js
+++ js/netforce/view/NetForceControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2018, University of Colorado Boulder
 
 /**
  * Control panel with options to show sum of forces and values, and also buttons below the control panel for reset all and sound.
