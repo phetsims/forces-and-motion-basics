b4eb9cb38caeecbf1485a4a214c174eea7895e3b
Jesse <jesse.greenberg@colorado.edu>
2018-10-24 18:27:47 -0400

    move strings to section, alphabetize, see #271

--- js/netforce/view/GoPauseButton.js
+++ js/netforce/view/GoPauseButton.js
@@ -9,25 +9,26 @@
 define( function( require ) {
   'use strict';
 
+  // modules
+  var BooleanToggleNode = require( 'SUN/BooleanToggleNode' );
   var DerivedProperty = require( 'AXON/DerivedProperty' );
   var Emitter = require( 'AXON/Emitter' );
   var EmitterIO = require( 'AXON/EmitterIO' );
   var forcesAndMotionBasics = require( 'FORCES_AND_MOTION_BASICS/forcesAndMotionBasics' );
-  var goString = require( 'string!FORCES_AND_MOTION_BASICS/go' );
   var inherit = require( 'PHET_CORE/inherit' );
   var NumberIO = require( 'TANDEM/types/NumberIO' );
-  var pauseString = require( 'string!FORCES_AND_MOTION_BASICS/pause' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var RoundPushButton = require( 'SUN/buttons/RoundPushButton' );
   var StringIO = require( 'TANDEM/types/StringIO' );
   var Text = require( 'SCENERY/nodes/Text' );
-  var BooleanToggleNode = require( 'SUN/BooleanToggleNode' );
 
   // strings
   var goButtonDescriptionString = require( 'string!FORCES_AND_MOTION_BASICS/goButton.description' );
+  var goString = require( 'string!FORCES_AND_MOTION_BASICS/go' );
   var pauseButtonDescriptionString = require( 'string!FORCES_AND_MOTION_BASICS/pauseButton.description' );
+  var pauseString = require( 'string!FORCES_AND_MOTION_BASICS/pause' );
 
   //Given nodes that have possibly different sizes, wrap the specified node in a parent empty Rectangle node so the bounds will match up
   //If the node is already the largest, don't wrap it.
