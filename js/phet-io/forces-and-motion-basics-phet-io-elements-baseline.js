018d6e6b296e1c10dbe7795f6a705a006dd14f5f
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:46 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
+++ js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
@@ -33587,7 +33587,7 @@ window.phet.phetio.phetioElementsBaseline =
     "forcesAndMotionBasics.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
