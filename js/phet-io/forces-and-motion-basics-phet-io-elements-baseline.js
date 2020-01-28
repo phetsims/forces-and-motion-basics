bd2c9c08c17e30781bb609a18601fdb9192bc681
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:57:55 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
+++ js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
@@ -27692,7 +27692,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "forcesAndMotionBasics.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
