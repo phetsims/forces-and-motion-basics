e55abddda413b3bcfe8d50882bc4d17842077c86
zepumph <michael.kauzmann@colorado.edu>
2020-02-28 13:35:25 -0900

    update documentation, https://github.com/phetsims/studio/issues/103

--- js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
+++ js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
@@ -27719,7 +27719,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "forcesAndMotionBasics.general.soundManager.enhancedSoundEnabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled. Note that not all simulations that support sound also support enhanced sound.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
