964adbf630110bc27b0b974d96ee5edb8e6ebecc
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 12:15:17 -0800

    rename mouseMovedAction -> mouseMoveAction, https://github.com/phetsims/scenery/issues/1043

--- js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
+++ js/phet-io/forces-and-motion-basics-phet-io-elements-baseline.js
@@ -25221,7 +25221,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<Vector2IO, EventIO>"
     },
-    "forcesAndMotionBasics.general.controller.input.mouseMovedAction": {
+    "forcesAndMotionBasics.general.controller.input.mouseMoveAction": {
       "phetioDocumentation": "Emits when the mouse is moved The arguments are:<br/><ol><li>point: Vector2IO</li><br/><li>event: EventIO</li></ol>",
       "phetioDynamicElement": false,
       "phetioEventType": "USER",
