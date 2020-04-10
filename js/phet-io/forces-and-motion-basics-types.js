00edb43a61a05181ce8cd7e1063d6f14f712f6e7
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 13:45:31 -0600

    revise IO Type doc for primitive core types, https://github.com/phetsims/tandem/issues/165

--- js/phet-io/forces-and-motion-basics-types.js
+++ js/phet-io/forces-and-motion-basics-types.js
@@ -244,7 +244,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ArrayIO<StringIO>"
     },
     "BooleanIO": {
-      "documentation": "Wrapper for the built-in JS boolean type (true/false)",
+      "documentation": "IO Type for Javascript's boolean primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -855,7 +855,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "NumberDisplayIO"
     },
     "NumberIO": {
-      "documentation": "Wrapper for the built-in JS number type (floating point, but also represents integers)",
+      "documentation": "IO Type for Javascript's number primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -1756,7 +1756,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "SliderIO"
     },
     "StringIO": {
-      "documentation": "Wrapper for the built-in JS string type",
+      "documentation": "IO Type for Javascript's string primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
