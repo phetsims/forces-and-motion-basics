79cb73bcbceb11439d537c8c4a806fd70cd4a6f7
zepumph <michael.kauzmann@colorado.edu>
2020-09-02 15:21:06 -0800

    prefer "IO Type" to lowercase "type", https://github.com/phetsims/chipper/issues/977

--- js/netforce/model/NetForceModelIO.js
+++ js/netforce/model/NetForceModelIO.js
@@ -1,7 +1,7 @@
 // Copyright 2017-2020, University of Colorado Boulder
 
 /**
- * IO type for NetForceModel
+ * IO Type for NetForceModel
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Andrew Adare (PhET Interactive Simulations)
