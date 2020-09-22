addd3b266f3aafb520affcddba61847e7f7285e7
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 17:40:36 -0600

    convert TextPushButton subclass to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/netforce/view/ReturnButton.js
+++ js/netforce/view/ReturnButton.js
@@ -6,38 +6,39 @@
  * @author Sam Reid
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import TextPushButton from '../../../../sun/js/buttons/TextPushButton.js';
-import forcesAndMotionBasicsStrings from '../../forcesAndMotionBasicsStrings.js';
 import forcesAndMotionBasics from '../../forcesAndMotionBasics.js';
+import forcesAndMotionBasicsStrings from '../../forcesAndMotionBasicsStrings.js';
 
 const returnString = forcesAndMotionBasicsStrings.return;
 
-/**
- * @param {NetForceModel} model
- * @param {Tandem} tandem
- * @param {Object} [options]
- * @constructor
- */
-function ReturnButton( model, tandem, options ) {
+class ReturnButton extends TextPushButton {
 
-  // TODO: this method bound the model. Why?
-  const returnCart = function() {
-    model.returnCart();
-  };
-  TextPushButton.call( this, returnString, {
-    listener: returnCart,
-    font: new PhetFont( { size: 16, weight: 'bold' } ),
-    baseColor: 'rgb( 254, 192, 0 )',
-    tandem: tandem
-  } );
-  this.mutate( options );
+  /**
+   * @param {NetForceModel} model
+   * @param {Tandem} tandem
+   * @param {Object} [options]
+   */
+  constructor( model, tandem, options ) {
 
-  model.startedProperty.linkAttribute( this, 'enabled' );
+    // TODO: this method bound the model. Why?
+    const returnCart = function() {
+      model.returnCart();
+    };
+
+    super( returnString, {
+      listener: returnCart,
+      font: new PhetFont( { size: 16, weight: 'bold' } ),
+      baseColor: 'rgb( 254, 192, 0 )',
+      tandem: tandem
+    } );
+
+    this.mutate( options );
+
+    model.startedProperty.linkAttribute( this, 'enabled' );
+  }
 }
 
 forcesAndMotionBasics.register( 'ReturnButton', ReturnButton );
-
-inherit( TextPushButton, ReturnButton );
 export default ReturnButton;
