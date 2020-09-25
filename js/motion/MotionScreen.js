b5c5c2792ae5f1f086c0a35be6d8d71063a0b270
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:34:29 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/motion/MotionScreen.js
+++ js/motion/MotionScreen.js
@@ -1,49 +1,55 @@
 // Copyright 2015-2020, University of Colorado Boulder
 
 /**
- *
- *
+ * TODO
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import forcesAndMotionBasics from '../forcesAndMotionBasics.js';
 import MotionModel from './model/MotionModel.js';
 import MotionScreenView from './view/MotionScreenView.js';
 
-/**
- * @param style
- * @param {Tandem} tandem
- * @constructor
- */
-function MotionScreen( style, tandem, options ) {
+class MotionScreen extends Screen {
 
-  options = options || {};
+  /**
+   * @param style
+   * @param {Tandem} tandem
+   * @param {Object} [options]
+   */
+  constructor( style, tandem, options ) {
 
-  assert && assert( !options.tandem, 'tandem is a constructor param, not an option' );
-  options.tandem = tandem;
+    options = options || {};
 
-  Screen.call( this,
-    function() {
-      return new MotionModel( style, tandem.createTandem( 'model' ) );
-    },
-    function( model ) {
-      return new MotionScreenView( model, tandem.createTandem( 'view' ) );
-    },
-    options );
+    assert && assert( !options.tandem, 'tandem is a constructor param, not an option' );
+    options.tandem = tandem;
+
+    super(
+      function() {
+        return new MotionModel( style, tandem.createTandem( 'model' ) );
+      },
+      function( model ) {
+        return new MotionScreenView( model, tandem.createTandem( 'view' ) );
+      },
+      options );
+  }
+
+  /**
+   * TODO
+   * @returns TODO
+   * @public
+   */
+  getState() {
+    return { model: this.model.getState(), view: {} };
+  }
+
+  /**
+   * TODO
+   * @public
+   */
+  setState() {
+  }
 }
 
 forcesAndMotionBasics.register( 'MotionScreen', MotionScreen );
-
-inherit( Screen, MotionScreen, {
-
-  getState: function() {
-    return { model: this.model.getState(), view: {} };
-  },
-
-  setState: function() {
-  }
-} );
-
 export default MotionScreen;
