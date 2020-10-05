6b52011b59cf136efd29f7758d283d9c5d297f6c
Jonathan Olson <jonathan.olson@colorado.edu>
2020-10-05 10:46:41 -0600

    Removing Image inherits for https://github.com/phetsims/joist/issues/664

--- js/netforce/view/CartNode.js
+++ js/netforce/view/CartNode.js
@@ -7,7 +7,6 @@
  */
 
 import Range from '../../../../dot/js/Range.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import GaugeNode from '../../../../scenery-phet/js/GaugeNode.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import cartImage from '../../../images/cart_png.js';
@@ -16,44 +15,40 @@ import forcesAndMotionBasics from '../../forcesAndMotionBasics.js';
 
 const speedString = forcesAndMotionBasicsStrings.speed;
 
-/**
- * Constructor.
- *
- * @param {Cart} cart
- * @param {NumberProperty} speedProperty
- * @param {Property.<Boolean>} showSpeedProperty
- * @param {Tandem} tandem
- * @constructor
- */
-function CartNode( cart, speedProperty, showSpeedProperty, tandem ) {
+class CartNode extends Image {
+  /**
+   * @param {Cart} cart
+   * @param {NumberProperty} speedProperty
+   * @param {Property.<Boolean>} showSpeedProperty
+   * @param {Tandem} tandem
+   */
+  constructor( cart, speedProperty, showSpeedProperty, tandem ) {
+    // super constructor
+    super( cartImage, {
+      y: 221,
+      tandem: tandem
+    } );
 
-  // super constructor
-  Image.call( this, cartImage, {
-    y: 221,
-    tandem: tandem
-  } );
+    this.cart = cart;
+    this.xPosition = this.cart.xProperty.get();
 
-  this.cart = cart;
-  this.xPosition = this.cart.xProperty.get();
+    // add a speedometer to the cart
+    const speedRange = new Range( 0, 6 ); // speed range of the cart in m/s
+    const speedometerNode = new GaugeNode( speedProperty, speedString, speedRange, {
+      centerX: this.centerX,
+      centerY: this.height / 2,
+      radius: this.width * 0.25,
+      majorTickLength: 8,
+      minorTickLength: 4,
+      majorTickLineWidth: 1,
+      maxLabelWidthScale: 1.0,
+      tandem: tandem.createTandem( 'speedometerNode' )
+    } );
 
-  // add a speedometer to the cart
-  const speedRange = new Range( 0, 6 ); // speed range of the cart in m/s
-  const speedometerNode = new GaugeNode( speedProperty, speedString, speedRange, {
-    centerX: this.centerX,
-    centerY: this.height / 2,
-    radius: this.width * 0.25,
-    majorTickLength: 8,
-    minorTickLength: 4,
-    majorTickLineWidth: 1,
-    maxLabelWidthScale: 1.0,
-    tandem: tandem.createTandem( 'speedometerNode' )
-  } );
-
-  showSpeedProperty.linkAttribute( speedometerNode, 'visible' );
-  this.addChild( speedometerNode );
+    showSpeedProperty.linkAttribute( speedometerNode, 'visible' );
+    this.addChild( speedometerNode );
+  }
 }
 
 forcesAndMotionBasics.register( 'CartNode', CartNode );
-
-inherit( Image, CartNode );
 export default CartNode;
