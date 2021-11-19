9bad6ff5a5531910ee6d8848b4e8b27eaf378576
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:28 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/forcesAndMotionBasicsStrings.ts
@@ -0,0 +1,51 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import forcesAndMotionBasics from './forcesAndMotionBasics.js';
+
+type StringsType = {
+  'none': string,
+  'force': string,
+  'values': string,
+  'redWins': string,
+  'frictionForce': string,
+  'pause': string,
+  'friction': string,
+  'forces-and-motion-basics': {
+    'title': string
+  },
+  'leftForce': string,
+  'appliedForce': string,
+  'rightForce': string,
+  'pattern': {
+    '0massUnitsKilograms': string,
+    '0valueUnitsNewtons': string,
+    '0name': {
+      '1valueUnitsAcceleration': string,
+      '1valueUnitsVelocity': string
+    },
+    '0valueUnitsN': string
+  },
+  'acceleration': string,
+  'lots': string,
+  'forces': string,
+  'netForce': string,
+  'motion': string,
+  'go': string,
+  'sumOfForcesEqualsZero': string,
+  'blueWins': string,
+  'sumOfForces': string,
+  'speed': string,
+  'return': string,
+  'masses': string
+};
+
+const forcesAndMotionBasicsStrings = getStringModule( 'FORCES_AND_MOTION_BASICS' ) as StringsType;
+
+forcesAndMotionBasics.register( 'forcesAndMotionBasicsStrings', forcesAndMotionBasicsStrings );
+
+export default forcesAndMotionBasicsStrings;
