4f974971abe4918f74e2a415e6c8d82eaab6ca23
phet-dev <phetadmin@gmail.com>
2022-03-05 03:22:58 -0700

    update copyright dates from daily grunt work

--- js/motion/view/AccelerometerNode.js
+++ js/motion/view/AccelerometerNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * In the 'Acceleration' screen, this horizontal indicator shows the amount of acceleration (like a moving bubble).
