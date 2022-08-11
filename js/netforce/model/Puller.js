8b0d61fa1cb60e77c409ae99b5ddd9174d877ec4
phet-dev <phetadmin@gmail.com>
2022-08-11 04:31:29 -0600

    update copyright dates from daily grunt work

--- js/netforce/model/Puller.js
+++ js/netforce/model/Puller.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Model for the red/blue pullers which can be dragged to the rope, and exert a force on the rope.
