b47079539744c1e9427ebf920df800b15516b0a6
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/forcesAndMotionBasicsStrings.ts
+++ js/forcesAndMotionBasicsStrings.ts
@@ -5,43 +5,72 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import forcesAndMotionBasics from './forcesAndMotionBasics.js';
 
 type StringsType = {
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'force': string;
+  'forceProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'redWins': string;
+  'redWinsProperty': TReadOnlyProperty<string>;
   'frictionForce': string;
+  'frictionForceProperty': TReadOnlyProperty<string>;
   'pause': string;
+  'pauseProperty': TReadOnlyProperty<string>;
   'friction': string;
+  'frictionProperty': TReadOnlyProperty<string>;
   'forces-and-motion-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'leftForce': string;
+  'leftForceProperty': TReadOnlyProperty<string>;
   'appliedForce': string;
+  'appliedForceProperty': TReadOnlyProperty<string>;
   'rightForce': string;
+  'rightForceProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0massUnitsKilograms': string;
+    '0massUnitsKilogramsProperty': TReadOnlyProperty<string>;
     '0valueUnitsNewtons': string;
+    '0valueUnitsNewtonsProperty': TReadOnlyProperty<string>;
     '0name': {
       '1valueUnitsAcceleration': string;
+      '1valueUnitsAccelerationProperty': TReadOnlyProperty<string>;
       '1valueUnitsVelocity': string;
+      '1valueUnitsVelocityProperty': TReadOnlyProperty<string>;
     };
     '0valueUnitsN': string;
+    '0valueUnitsNProperty': TReadOnlyProperty<string>;
   };
   'acceleration': string;
+  'accelerationProperty': TReadOnlyProperty<string>;
   'lots': string;
+  'lotsProperty': TReadOnlyProperty<string>;
   'forces': string;
+  'forcesProperty': TReadOnlyProperty<string>;
   'netForce': string;
+  'netForceProperty': TReadOnlyProperty<string>;
   'motion': string;
+  'motionProperty': TReadOnlyProperty<string>;
   'go': string;
+  'goProperty': TReadOnlyProperty<string>;
   'sumOfForcesEqualsZero': string;
+  'sumOfForcesEqualsZeroProperty': TReadOnlyProperty<string>;
   'blueWins': string;
+  'blueWinsProperty': TReadOnlyProperty<string>;
   'sumOfForces': string;
+  'sumOfForcesProperty': TReadOnlyProperty<string>;
   'speed': string;
+  'speedProperty': TReadOnlyProperty<string>;
   'return': string;
+  'returnProperty': TReadOnlyProperty<string>;
   'masses': string;
+  'massesProperty': TReadOnlyProperty<string>;
 };
 
 const forcesAndMotionBasicsStrings = getStringModule( 'FORCES_AND_MOTION_BASICS' ) as StringsType;
