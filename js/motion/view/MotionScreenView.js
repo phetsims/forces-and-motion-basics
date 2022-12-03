8a30fdda21696dfe647b4e55c23ea1a9f71630f0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2022-12-02 17:13:22 -0700

    NumberProperty.range and rangeProperty cannot be of type null, https://github.com/phetsims/axon/issues/425

--- js/motion/view/MotionScreenView.js
+++ js/motion/view/MotionScreenView.js
@@ -149,7 +149,7 @@ class MotionScreenView extends ScreenView {
       spinnerRange.min = enableLeftButtons ? -500 : 0;
     } );
 
-    const appliedForceSpinner = new FineCoarseSpinner( model.appliedForceProperty.asRanged(), {
+    const appliedForceSpinner = new FineCoarseSpinner( model.appliedForceProperty, {
       numberDisplayOptions: {
         valuePattern: pattern0ValueUnitsNewtonsString,
         align: 'center',
