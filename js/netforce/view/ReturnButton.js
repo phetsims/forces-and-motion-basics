a55e18c397accec8d5135fce734d9dae36040998
phet-dev <phetadmin@gmail.com>
2023-09-13 03:14:27 -0600

    update copyright dates from daily grunt work

--- js/netforce/view/ReturnButton.js
+++ js/netforce/view/ReturnButton.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * Shows a button that allows the user to "return" the cart after a match has completed.
