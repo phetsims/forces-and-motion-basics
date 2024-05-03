c69d3fd72e7fb7b8296b9b9402d3a5004bf7ff76
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-05-03 10:01:51 -0600

    Multilink listener updates the NetForceControlPanel.js layout when string properties change. See https://github.com/phetsims/forces-and-motion-basics/issues/309.

--- js/netforce/view/NetForceControlPanel.js
+++ js/netforce/view/NetForceControlPanel.js
@@ -6,6 +6,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import Multilink from '../../../../axon/js/Multilink.js';
 import merge from '../../../../phet-core/js/merge.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import SoundToggleButton from '../../../../scenery-phet/js/buttons/SoundToggleButton.js';
@@ -14,11 +15,17 @@ import { HBox, Node, Text } from '../../../../scenery/js/imports.js';
 import Panel from '../../../../sun/js/Panel.js';
 import VerticalCheckboxGroup from '../../../../sun/js/VerticalCheckboxGroup.js';
 import ForcesAndMotionBasicsIconFactory from '../../common/view/ForcesAndMotionBasicsIconFactory.js';
+import ForcesAndMotionBasicsLayoutBounds from '../../common/view/ForcesAndMotionBasicsLayoutBounds.js';
 import forcesAndMotionBasics from '../../forcesAndMotionBasics.js';
 import ForcesAndMotionBasicsStrings from '../../ForcesAndMotionBasicsStrings.js';
 
 // constants
 const BUTTON_PADDING = 7; // placement padding for the reset all button and the mute button
+
+// strings
+const speedStringProperty = ForcesAndMotionBasicsStrings.speedStringProperty;
+const sumOfForcesStringProperty = ForcesAndMotionBasicsStrings.sumOfForcesStringProperty;
+const valuesStringProperty = ForcesAndMotionBasicsStrings.valuesStringProperty;
 
 class NetForceControlPanel extends Node {
   /**
@@ -40,15 +47,15 @@ class NetForceControlPanel extends Node {
 
     // Create a separate font options to account for the speedometerIconNode and speedTextAndSpeedometerIconSpacing
     const speedFontOptions = { font: fontOptions.font, maxWidth: fontOptions.maxWidth - speedometerIconNode.width - speedTextAndSpeedometerIconSpacing };
-    const showSpeedTextNode = new Text( ForcesAndMotionBasicsStrings.speedStringProperty, merge( { tandem: tandem.createTandem( 'showSpeedText' ) }, speedFontOptions ) );
+    const showSpeedTextNode = new Text( speedStringProperty, merge( { tandem: tandem.createTandem( 'showSpeedText' ) }, speedFontOptions ) );
 
     const verticalCheckboxGroupTandem = tandem.createTandem( 'verticalCheckboxGroup' );
     this.verticalCheckboxGroup = new VerticalCheckboxGroup( [ {
-      createNode: tandem => new Text( ForcesAndMotionBasicsStrings.sumOfForcesStringProperty, merge( { tandem: tandem.createTandem( 'showSumOfForcesText' ) }, fontOptions ) ),
+      createNode: tandem => new Text( sumOfForcesStringProperty, merge( { tandem: tandem.createTandem( 'showSumOfForcesText' ) }, fontOptions ) ),
       property: model.showSumOfForcesProperty,
       tandemName: 'showSumOfForcesCheckbox'
     }, {
-      createNode: tandem => new Text( ForcesAndMotionBasicsStrings.valuesStringProperty, merge( { tandem: tandem.createTandem( 'showValuesText' ) }, fontOptions ) ),
+      createNode: tandem => new Text( valuesStringProperty, merge( { tandem: tandem.createTandem( 'showValuesText' ) }, fontOptions ) ),
       property: model.showValuesProperty,
       tandemName: 'showValuesCheckbox'
     }, {
@@ -60,7 +67,8 @@ class NetForceControlPanel extends Node {
       property: model.showSpeedProperty,
       tandemName: 'showSpeedCheckbox'
     } ], {
-      tandem: verticalCheckboxGroupTandem
+      tandem: verticalCheckboxGroupTandem,
+      minContentWidth: 100
     } );
     const verticalCheckboxGroupPanel = new Panel( this.verticalCheckboxGroup, {
       xMargin: 10,
@@ -76,7 +84,6 @@ class NetForceControlPanel extends Node {
         model.reset();
       },
       radius: 23,
-      rightCenter: verticalCheckboxGroupPanel.rightBottom.plusXY( -BUTTON_PADDING, 35 ),
       tandem: tandem.createTandem( 'resetAllButton' )
     } );
     this.addChild( this.resetAllButton );
@@ -84,19 +91,23 @@ class NetForceControlPanel extends Node {
     const soundToggleButton = new SoundToggleButton( model.volumeOnProperty, {
       padX: 19,
       padY: 19,
-      centerY: this.resetAllButton.centerY,
       tandem: tandem.createTandem( 'soundToggleButton' )
     } );
 
-    // layout
-    soundToggleButton.left = verticalCheckboxGroupPanel.left + BUTTON_PADDING;
-
-    // i18n - if the strings are too short, the sound toggle button will overlap the reset all button, add some padding
-    if ( this.resetAllButton.left < soundToggleButton.right ) {
-      soundToggleButton.right = this.resetAllButton.left - 2 * BUTTON_PADDING;
-    }
-
     this.addChild( soundToggleButton );
+
+    // Update the layout to support dynamic locale
+    Multilink.multilink( [ sumOfForcesStringProperty, valuesStringProperty, speedStringProperty ], () => {
+      verticalCheckboxGroupPanel.right = ForcesAndMotionBasicsLayoutBounds.width - 5;
+      this.resetAllButton.rightCenter = verticalCheckboxGroupPanel.rightBottom.plusXY( -BUTTON_PADDING, 35 );
+      soundToggleButton.left = verticalCheckboxGroupPanel.left + BUTTON_PADDING;
+      soundToggleButton.centerY = this.resetAllButton.centerY;
+
+      // i18n - if the strings are too short, the sound toggle button will overlap the reset all button, add some padding
+      if ( this.resetAllButton.left < soundToggleButton.right ) {
+        soundToggleButton.right = this.resetAllButton.left - 2 * BUTTON_PADDING;
+      }
+    } );
   }
 }
 
