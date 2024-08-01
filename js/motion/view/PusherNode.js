802f16c438b5662e89987eadbda2ec6708cdbcf7
phet-dev <phetadmin@gmail.com>
2024-08-01 03:43:59 -0600

    update copyright dates from daily grunt work

--- js/motion/view/PusherNode.js
+++ js/motion/view/PusherNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2023, University of Colorado Boulder
+// Copyright 2013-2024, University of Colorado Boulder
 
 /**
  * Shows the draggable pusher, which applies force to the objects in the center of the screen and falls down if he exceeds the maximum velocity.
