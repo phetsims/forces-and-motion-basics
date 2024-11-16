aa714d90bb5a54c13e7df96f8e7c646052c7a49c
phet-dev <phetadmin@gmail.com>
2024-11-16 03:25:25 -0700

    update copyright dates from daily grunt work

--- js/netforce/view/CartStopperNode.ts
+++ js/netforce/view/CartStopperNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * Constructs a shape that looks like trapezoid.  This shape is to be placed in the screen view at a place that
