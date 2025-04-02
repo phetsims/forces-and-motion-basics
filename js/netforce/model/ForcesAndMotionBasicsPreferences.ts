aafb1a4a2f637b6e4e26aa54f74c3f65c760c123
phet-dev <phetadmin@gmail.com>
2025-04-02 03:42:41 -0600

    update copyright dates from daily grunt work

--- js/netforce/model/ForcesAndMotionBasicsPreferences.ts
+++ js/netforce/model/ForcesAndMotionBasicsPreferences.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * ForcesAndMotionBasicsPreferences is the model for sim-specific preferences.  The values declared here can be updated via
