a2d95a8981452218dcdaa650c389cb4cceed8bdc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-04-02 10:39:40 -0600

    overrides must be JSON (double quotes) ((ew)), https://github.com/phetsims/forces-and-motion-basics/issues/349

--- js/forces-and-motion-basics-phet-io-overrides.js
+++ js/forces-and-motion-basics-phet-io-overrides.js
@@ -1,109 +1,108 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides =
-  {
-    'forcesAndMotionBasics.accelerationScreen.model.visibleProperties.showAccelerationProperty': {
-      'phetioFeatured': true
+window.phet.preloads.phetio.phetioElementsOverrides = {
+    "forcesAndMotionBasics.accelerationScreen.model.visibleProperties.showAccelerationProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.frictionScreen.model.visibleProperties.showSumOfForcesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.frictionScreen.model.visibleProperties.showSumOfForcesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.accelerationProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.accelerationProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.forces.frictionCoefficientProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.forces.frictionCoefficientProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.forces.frictionForceProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.forces.frictionForceProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.forces.sumOfForcesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.forces.sumOfForcesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.isPlayingProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.isPlayingProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.items.crate1.inStackProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.items.crate1.inStackProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.pusher.fallenProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.pusher.fallenProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.speedProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.speedProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.stackedItems': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.stackedItems": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.stackedItems.lengthProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.stackedItems.lengthProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.stopwatch': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.stopwatch": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.velocityProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.velocityProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.visibleProperties.showForceProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.visibleProperties.showForceProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.visibleProperties.showMassesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.visibleProperties.showMassesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.visibleProperties.showSpeedProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.visibleProperties.showSpeedProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.model.visibleProperties.showValuesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.model.visibleProperties.showValuesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.view.appliedForceControl.spinner.visibleProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.view.appliedForceControl.spinner.visibleProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.motionScreen.view.items.crate1Node.visibleProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.motionScreen.view.items.crate1Node.visibleProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.cart.positionProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.cart.positionProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.cart.velocityProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.cart.velocityProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.forces.leftForceProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.forces.leftForceProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.forces.netForceProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.forces.netForceProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.forces.rightForceProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.forces.rightForceProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.hasStartedProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.hasStartedProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.isRunningProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.isRunningProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.pullers.largeLeftPuller.knotProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.pullers.largeLeftPuller.knotProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.speedProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.speedProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.visibleProperties.showSpeedProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.visibleProperties.showSpeedProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.visibleProperties.showSumOfForcesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.visibleProperties.showSumOfForcesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.model.visibleProperties.showValuesProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.model.visibleProperties.showValuesProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.view.controlPanel.checkboxGroup.visibleProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.view.controlPanel.checkboxGroup.visibleProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.view.controlPanel.visibleProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.view.controlPanel.visibleProperty": {
+      "phetioFeatured": true
     },
-    'forcesAndMotionBasics.netForceScreen.view.pullers.largeLeftPullerNode.visibleProperty': {
-      'phetioFeatured': true
+    "forcesAndMotionBasics.netForceScreen.view.pullers.largeLeftPullerNode.visibleProperty": {
+      "phetioFeatured": true
     }
   };
