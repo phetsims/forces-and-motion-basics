7e7cd44e4816f4cb59ef521b87719cc9b1465be6
Sam Reid <reids@colorado.edu>
2025-07-28 20:35:39 -0600

    Add description, see https://github.com/phetsims/forces-and-motion-basics/issues/378

--- js/motion/view/SpeedometerNode.ts
+++ js/motion/view/SpeedometerNode.ts
@@ -10,6 +10,7 @@
 import Multilink from '../../../../axon/js/Multilink.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Range from '../../../../dot/js/Range.js';
+import Utils from '../../../../dot/js/Utils.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
 import ValueGaugeNode from '../../../../scenery-phet/js/ValueGaugeNode.js';
@@ -64,34 +65,34 @@ export default class SpeedometerNode extends Node {
     // Add accessibility descriptions
     // MotionConstants.MAX_SPEED is 40 m/s
     Multilink.multilink( [ speedProperty, showValuesProperty ], ( speed, showValues ) => {
-      
+
       // Build the accessible paragraph description
       let description = 'A speedometer shows the speed.';
-      
+
       // Add qualitative speed description
       const absSpeed = Math.abs( speed );
       const speedDescriptor = absSpeed === 0 ? 'stopped' :
-                             absSpeed < 2 ? 'very slow' :
-                             absSpeed < 5 ? 'slow' :
-                             absSpeed < 10 ? 'moderate' :
-                             absSpeed < 20 ? 'fast' :
-                             absSpeed < 30 ? 'very fast' :
-                             'extremely fast';
-      
+                              absSpeed < 2 ? 'very slow' :
+                              absSpeed < 5 ? 'slow' :
+                              absSpeed < 10 ? 'moderate' :
+                              absSpeed < 20 ? 'fast' :
+                              absSpeed < 30 ? 'very fast' :
+                              'extremely fast';
+
       if ( absSpeed === 0 ) {
         description += ' The speed is zero';
       }
       else {
         description += ` The speed is ${speedDescriptor}`;
       }
-      
+
       // Add exact value if shown
       if ( showValues ) {
-        description += ` at ${absSpeed.toFixed( 1 )} meters per second`;
+        description += ` at ${Utils.toFixed( absSpeed, 1 )} meters per second`;
       }
-      
+
       description += '.';
-      
+
       this.accessibleParagraph = description;
     } );
 
