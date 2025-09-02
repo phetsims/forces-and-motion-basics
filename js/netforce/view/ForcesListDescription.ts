88ea7f0aa4f015bd988d28afacce7b9167ff5f44
Sam Reid <reids@colorado.edu>
2025-09-02 14:34:59 -0600

    Move description to constructor and derived property instances, see https://github.com/phetsims/forces-and-motion-basics/issues/417

--- js/netforce/view/ForcesListDescription.ts
+++ js/netforce/view/ForcesListDescription.ts
@@ -7,7 +7,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import AccessibleListNode from '../../../../scenery-phet/js/accessibility/AccessibleListNode.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import getQualitativeForceDescription from '../../common/view/getQualitativeForceDescription.js';
@@ -16,90 +16,98 @@ import ForcesAndMotionBasicsFluent from '../../ForcesAndMotionBasicsFluent.js';
 import NetForceModel from '../model/NetForceModel.js';
 
 export default class ForcesListDescription extends Node {
-
-  private forcesList: AccessibleListNode | null = null;
+  private readonly forcesList: AccessibleListNode | null = null;
 
   public constructor( private readonly model: NetForceModel ) {
+
+    const THRESHOLD = 1e-6;
+
+    // Visibility: show this container only if any section is visible.
+    const leftVisibleProperty = new DerivedProperty( [ model.leftForceProperty ], left => Math.abs( left ) > THRESHOLD );
+    const rightVisibleProperty = new DerivedProperty( [ model.rightForceProperty ], right => Math.abs( right ) > THRESHOLD );
+    const anyVisibleProperty = DerivedProperty.or( [ leftVisibleProperty, rightVisibleProperty, model.showSumOfForcesProperty ] );
+
     super( {
       tagName: 'div',
-      accessibleHeading: ForcesAndMotionBasicsFluent.a11y.forces.headingStringProperty
+      accessibleHeading: ForcesAndMotionBasicsFluent.a11y.forces.headingStringProperty,
+      visibleProperty: anyVisibleProperty
     } );
 
-    // Update the forces list when any relevant property changes
-    const updateListener = () => this.updateForcesList();
+    // Keep disposables tied to this Node lifecycle
 
-    this.model.leftForceProperty.link( updateListener );
-    this.model.rightForceProperty.link( updateListener );
-    this.model.netForceProperty.link( updateListener );
-    this.model.showValuesProperty.link( updateListener );
-    this.model.showSumOfForcesProperty.link( updateListener );
+    // Dependencies for qualitative descriptions so language changes recompute values.
+    const qualitativeStringDependencies = [
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.smallStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.mediumSmallStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.mediumStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.mediumLargeStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.largeStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.veryLargeStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.extremelyLargeStringProperty
+    ];
 
-    // Initial update
-    this.updateForcesList();
-  }
+    // LEFT item description text (without the "left force" wrapper)
+    const leftDescriptionProperty = DerivedProperty.deriveAny( [
+      model.leftForceProperty,
+      model.showValuesProperty,
+      ...ForcesAndMotionBasicsFluent.a11y.forces.quantitativeDescription.getDependentProperties(),
+      ...qualitativeStringDependencies
+    ], () => this.getForceDescription( model.leftForceProperty.value, model.showValuesProperty.value ) );
 
-  /**
-   * Updates the forces list content based on current force values and checkbox states
-   */
-  private updateForcesList(): void {
-    // Remove existing list
-    this.removeAllChildren();
+    // RIGHT item description text
+    const rightDescriptionProperty = DerivedProperty.deriveAny( [
+      model.rightForceProperty,
+      model.showValuesProperty,
+      ...ForcesAndMotionBasicsFluent.a11y.forces.quantitativeDescription.getDependentProperties(),
+      ...qualitativeStringDependencies
+    ], () => this.getForceDescription( model.rightForceProperty.value, model.showValuesProperty.value ) );
 
-    const forceDescriptions: TReadOnlyProperty<string>[] = [];
-    const leftForce = this.model.leftForceProperty.value;
-    const rightForce = this.model.rightForceProperty.value;
-    const netForce = this.model.netForceProperty.value;
-    const showValues = this.model.showValuesProperty.value;
-    const showSumOfForces = this.model.showSumOfForcesProperty.value;
+    // SUM item description text (magnitude only)
+    const sumMagnitudeDescriptionProperty = DerivedProperty.deriveAny( [
+      model.netForceProperty,
+      model.showValuesProperty,
+      ...ForcesAndMotionBasicsFluent.a11y.forces.quantitativeDescription.getDependentProperties(),
+      ...qualitativeStringDependencies
+    ], () => this.getForceDescription( model.netForceProperty.value, model.showValuesProperty.value ) );
 
-    // Add left force if non-zero
-    if ( Math.abs( leftForce ) > 1E-6 ) {
-      const description = this.getForceDescription( leftForce, showValues );
-      const leftForceProperty = ForcesAndMotionBasicsFluent.a11y.forces.leftForceArrow.createProperty( {
-        description: description
-      } );
-      forceDescriptions.push( leftForceProperty );
-    }
+    // SUM item direction string (localized left/right)
+    const sumDirectionProperty = DerivedProperty.deriveAny( [
+      model.netForceProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.leftStringProperty,
+      ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.rightStringProperty
+    ], () => model.netForceProperty.value >= 0 ?
+             ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.rightStringProperty.value :
+             ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.leftStringProperty.value );
 
-    // Add right force if non-zero
-    if ( Math.abs( rightForce ) > 1E-6 ) {
-      const description = this.getForceDescription( rightForce, showValues );
-      const rightForceProperty = ForcesAndMotionBasicsFluent.a11y.forces.rightForceArrow.createProperty( {
-        description: description
-      } );
-      forceDescriptions.push( rightForceProperty );
-    }
+    // Wrap LEFT/RIGHT descriptions with arrow labels.
+    const leftItemStringProperty = ForcesAndMotionBasicsFluent.a11y.forces.leftForceArrow.createProperty( {
+      description: leftDescriptionProperty
+    } );
+    const rightItemStringProperty = ForcesAndMotionBasicsFluent.a11y.forces.rightForceArrow.createProperty( {
+      description: rightDescriptionProperty
+    } );
 
-    // Add sum of forces if checkbox is checked
-    if ( showSumOfForces ) {
-      if ( Math.abs( netForce ) < 1E-6 ) {
-        // Special case: sum = 0
-        forceDescriptions.push( ForcesAndMotionBasicsFluent.a11y.forces.sumOfForcesZeroStringProperty );
-      }
-      else {
-        const description = this.getForceDescription( netForce, showValues );
-        const direction = netForce > 0 ?
-                          ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.rightStringProperty.value :
-                          ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.leftStringProperty.value;
+    // SUM item: choose between zero text and arrow text based on net force value, but always controlled by checkbox visibility
+    const sumIsZeroProperty = new DerivedProperty( [ model.netForceProperty ], net => Math.abs( net ) < THRESHOLD );
 
-        // TODO: Does this need diposal? See https://github.com/phetsims/forces-and-motion-basics/issues/398
-        const sumForceProperty = ForcesAndMotionBasicsFluent.a11y.forces.sumOfForcesArrow.createProperty( {
-          description: description,
-          direction: direction
-        } );
-        forceDescriptions.push( sumForceProperty );
-      }
-    }
+    const sumArrowStringProperty = ForcesAndMotionBasicsFluent.a11y.forces.sumOfForcesArrow.createProperty( {
+      description: sumMagnitudeDescriptionProperty,
+      direction: sumDirectionProperty
+    } );
 
-    // Create the list if we have any items, otherwise hide the component
-    if ( forceDescriptions.length > 0 ) {
-      this.forcesList = new AccessibleListNode( forceDescriptions );
-      this.addChild( this.forcesList );
-      this.visible = true;
-    }
-    else {
-      this.visible = false;
-    }
+    const sumZeroStringProperty = ForcesAndMotionBasicsFluent.a11y.forces.sumOfForcesZeroStringProperty;
+
+    const sumItemStringProperty = new DerivedProperty( [ sumIsZeroProperty, sumZeroStringProperty, sumArrowStringProperty ],
+      () => sumIsZeroProperty.value ? sumZeroStringProperty.value : sumArrowStringProperty.value );
+
+    // Build the AccessibleListNode once with three potential items, each with its own visibility
+    this.forcesList = new AccessibleListNode( [
+      { stringProperty: leftItemStringProperty, visibleProperty: leftVisibleProperty },
+      { stringProperty: rightItemStringProperty, visibleProperty: rightVisibleProperty },
+      { stringProperty: sumItemStringProperty, visibleProperty: model.showSumOfForcesProperty }
+    ] );
+
+    this.addChild( this.forcesList );
   }
 
   /**
@@ -110,9 +118,9 @@ export default class ForcesListDescription extends Node {
 
     if ( showValues ) {
       // Quantitative description with newtons using Fluent pattern
-      return ForcesAndMotionBasicsFluent.a11y.forces.quantitativeDescription.createProperty( {
+      return ForcesAndMotionBasicsFluent.a11y.forces.quantitativeDescription.format( {
         forceMagnitude: magnitude.toString()
-      } ).value;
+      } );
     }
     else {
       // Qualitative description using same thresholds as ReadoutArrow
