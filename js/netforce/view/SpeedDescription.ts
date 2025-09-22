989df740a71a5df4f4379c147939031570b83ee6
Jesse <jesse.greenberg@colorado.edu>
2025-09-22 16:02:31 -0400

    Revert "testing using descriptionContent instead of accessibleParagraph for https://github.com/phetsims/forces-and-motion-basics/issues/443"
    This reverts commit c18a9e712f7ad855607a190ba01d84d276fa7611.

--- js/netforce/view/SpeedDescription.ts
+++ js/netforce/view/SpeedDescription.ts
@@ -82,8 +82,7 @@ export default class SpeedDescription extends Node {
     super( {
       tagName: 'div',
       accessibleHeading: ForcesAndMotionBasicsFluent.a11y.speed.headingStringProperty,
-      descriptionTagName: 'p',
-      descriptionContent: speedDescriptionProperty
+      accessibleParagraph: 'This is a string'
     } );
 
     this.speedDescriptionProperty = speedDescriptionProperty;
