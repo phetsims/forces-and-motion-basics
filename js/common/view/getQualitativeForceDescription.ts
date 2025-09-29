1b9ee8aa3d05dbc200e3508819fc59dc6b3b8749
Sam Reid <reids@colorado.edu>
2025-09-29 05:48:06 -0600

    Address review comments, see https://github.com/phetsims/forces-and-motion-basics/issues/451

--- js/common/view/getQualitativeForceDescription.ts
+++ js/common/view/getQualitativeForceDescription.ts
@@ -1,12 +1,8 @@
 // Copyright 2025, University of Colorado Boulder
 
-//REVIEW Having one function all by itself feels a little sad. Are there not other force-related description methods
-//REVIEW   that could be bundled into a ForceDescriber class?
-
-//REVIEW How does this use "the same thresholds as ReadoutArrow"? Magnitude thresholds are hard-coded herein, and I see no code sharing with ReadoutArrow.
 /**
- * Utility function to get qualitative force descriptions based on magnitude.
- * Uses the same thresholds as ReadoutArrow for consistency across the simulation.
+ * Utility function to get qualitative force descriptions based on magnitude. This is one of the few parts where the
+ * values and descriptions from the netforce screen is shared with the motion screens.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -16,26 +12,24 @@ import ForcesAndMotionBasicsFluent from '../../ForcesAndMotionBasicsFluent.js';
 /**
  * Returns a qualitative description string for a force magnitude.
  * Uses threshold-based descriptors that work for both netforce (quantized) and motion (continuous) values.
- * @param magnitude - The absolute value of the force
- * @returns A qualitative description string (e.g., "small", "medium", "large")
  */
-export default function getQualitativeForceDescription( magnitude: number ): string {
-  if ( magnitude < 75 ) {
+export default function getQualitativeForceDescription( forceMagnitude: number ): string {
+  if ( forceMagnitude < 75 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.verySmallStringProperty.value;
   }
-  else if ( magnitude < 125 ) {
+  else if ( forceMagnitude < 125 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.smallStringProperty.value;
   }
-  else if ( magnitude < 175 ) {
+  else if ( forceMagnitude < 175 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.mediumStringProperty.value;
   }
-  else if ( magnitude < 225 ) {
+  else if ( forceMagnitude < 225 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.somewhatLargeStringProperty.value;
   }
-  else if ( magnitude < 275 ) {
+  else if ( forceMagnitude < 275 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.largeStringProperty.value;
   }
-  else if ( magnitude < 325 ) {
+  else if ( forceMagnitude < 325 ) {
     return ForcesAndMotionBasicsFluent.a11y.forces.qualitativeDescriptions.veryLargeStringProperty.value;
   }
   else {
