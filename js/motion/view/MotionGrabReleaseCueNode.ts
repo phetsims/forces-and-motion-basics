aa08960c8fe60b2249cd96a56f21a01d2c572d56
Sam Reid <reids@colorado.edu>
2025-10-01 13:50:18 -0600

    Fix the position of the cue node, see https://github.com/phetsims/forces-and-motion-basics/issues/461

--- js/motion/view/MotionGrabReleaseCueNode.ts
+++ js/motion/view/MotionGrabReleaseCueNode.ts
@@ -7,9 +7,9 @@
  */
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
+import FocusDrivenGrabReleaseCueNode from '../../common/view/FocusDrivenGrabReleaseCueNode.js';
 import forcesAndMotionBasics from '../../forcesAndMotionBasics.js';
 import ItemNode from './ItemNode.js';
-import FocusDrivenGrabReleaseCueNode from '../../common/view/FocusDrivenGrabReleaseCueNode.js';
 
 export default class MotionGrabReleaseCueNode extends FocusDrivenGrabReleaseCueNode<ItemNode> {
   public constructor( itemNodes: ItemNode[], layoutBounds: Bounds2, tandem?: Tandem ) {
@@ -19,8 +19,11 @@ export default class MotionGrabReleaseCueNode extends FocusDrivenGrabReleaseCueN
       ( self, focusedItemNode, bounds ) => {
         const item = focusedItemNode.item;
         const side = item.modeProperty.value === 'onStack' ? 'stack' : item.getToolboxSide();
-        self.centerX = side === 'stack' ? bounds.centerX : ( side === 'left' ? bounds.width * 0.25 : bounds.width * 0.75 );
-        self.top = 200;
+
+        const sideFraction = 0.165;
+        self.centerX = side === 'stack' ? bounds.centerX : ( side === 'left' ? bounds.width * sideFraction : bounds.width * ( 1 - sideFraction ) );
+
+        self.top = side === 'stack' ? 185 : 314;
       },
       tandem
     );
