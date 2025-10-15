39b725dae6b116da35f861a092cf09212fff7eca
Sam Reid <reids@colorado.edu>
2025-10-15 16:39:50 -0600

    Show the grab cue node over an attached puller's head, see https://github.com/phetsims/forces-and-motion-basics/issues/471

--- js/netforce/view/NetForceGrabReleaseCueNode.ts
+++ js/netforce/view/NetForceGrabReleaseCueNode.ts
@@ -10,9 +10,10 @@
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
+import FocusDrivenGrabReleaseCueNode from '../../common/view/FocusDrivenGrabReleaseCueNode.js';
+import ForcesAndMotionBasicsLayoutBounds from '../../common/view/ForcesAndMotionBasicsLayoutBounds.js';
 import forcesAndMotionBasics from '../../forcesAndMotionBasics.js';
 import PullerNode from './PullerNode.js';
-import FocusDrivenGrabReleaseCueNode from '../../common/view/FocusDrivenGrabReleaseCueNode.js';
 
 export default class NetForceGrabReleaseCueNode extends FocusDrivenGrabReleaseCueNode<PullerNode> {
   public constructor( pullerNodes: PullerNode[], layoutBounds: Bounds2, tandem?: Tandem ) {
@@ -20,9 +21,27 @@ export default class NetForceGrabReleaseCueNode extends FocusDrivenGrabReleaseCu
       pullerNodes,
       layoutBounds,
       ( self, focusedPullerNode, bounds ) => {
-        const isLeftSide = focusedPullerNode.puller.type === 'blue';
-        self.centerX = isLeftSide ? bounds.width * 0.2 : bounds.width * 0.8;
-        self.top = 325;
+
+        // Center over a puller on the rope
+        if ( focusedPullerNode.puller.modeProperty.value.isAttached() ) {
+          self.centerX = focusedPullerNode.centerX;
+          self.bottom = focusedPullerNode.top - 10;
+
+          // But don't go out of bounds
+          if ( self.left < 10 ) {
+            self.left = 10;
+          }
+          if ( self.right > ForcesAndMotionBasicsLayoutBounds.right - 10 ) {
+            self.right = ForcesAndMotionBasicsLayoutBounds.right - 10;
+          }
+        }
+        else {
+
+          // in the toolbox, just show a non-moving one that is in a good spot
+          const isLeftSide = focusedPullerNode.puller.type === 'blue';
+          self.centerX = isLeftSide ? bounds.width * 0.2 : bounds.width * 0.8;
+          self.top = 325;
+        }
       },
       tandem
     );
