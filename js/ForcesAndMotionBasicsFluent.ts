1ce22ce849f0a0c7f0ec834935323b316808b3c2
phet-dev <phetadmin@gmail.com>
2026-01-08 03:46:09 -0700

    update copyright dates from daily grunt work

--- js/ForcesAndMotionBasicsFluent.ts
+++ js/ForcesAndMotionBasicsFluent.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 // AUTOMATICALLY GENERATED – DO NOT EDIT.
 // Generated from forces-and-motion-basics-strings_en.yaml
 
